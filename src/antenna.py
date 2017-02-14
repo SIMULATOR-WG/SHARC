8de112e1033d3f1acb5e43f5f8ee290408a31dba
edgar <edgar@anatel.gov.br>
2017-02-14 11:31:37 -0200

    Using type hinting Including a remainder to check the overridden operators in case of directional antennas

--- src/antenna.py
+++ src/antenna.py
@@ -7,16 +7,21 @@ Created on Mon Feb  6 10:38:20 2017
 
 class Antenna(object):
     
-    def __init__(self, gain=0):
+    def __init__(self, gain: float = 0):
         self.__gain = gain
         pass
     
-    def set_gain(self, gain):
+    def set_gain(self, gain: float, angle: float = 0):
         self.__gain = gain
     
-    def get_gain(self):
+    def get_gain(self, angle: float = 0) -> float:
         return self.__gain
 
+    """
+    TODO: check the validity of operator overriding for Antenna class because
+    it has to take into account the departure/arrival angle. Consider the 
+    posibility of creating an arrival angle attribute (instead of parameter)
+    """
     def __float__(self):
         return float(self.get_gain())
         
