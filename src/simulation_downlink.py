35365b31f170c3545ea4b76e5d7e3b75e97e368f
edgar <edgar@anatel.gov.br>
2017-02-16 19:06:03 -0200

    Moving the coordinates creation code to station_manager

--- src/simulation_downlink.py
+++ src/simulation_downlink.py
@@ -5,76 +5,71 @@ Created on Wed Jan 11 19:06:41 2017
 @author: edgar
 """
 
-import numpy
-import math
+import numpy as np
 
 from simulation import Simulation
-from parameters import Parameters
+from parameters.parameters_imt import ParametersImt
 from station_manager import StationManager
-#from SatelliteReceiver import SatelliteReceiver
+from macrocell_topology import MacrocellTopology
 
 class SimulationDownlink(Simulation):
+    """
+    Implements the flowchart of simulation downlink method
+    """
     
     def __init__(self):
-        self.imt_base_stations = BaseStations()
-#        self.satellite_receiver = SatelliteReceiver()
+        self.__bs = ParametersImt.num_base_stations
+        self.__c = ParametersImt.num_clusters
+        self.__k = ParametersImt.ue_k
+        self.__m = ParametersImt.ue_k_m
+        
+        self.__topology = MacrocellTopology(ParametersImt.intersite_distance,
+                                    ParametersImt.num_clusters)
+
+        self.reset()
     
+    def reset(self):
+        #self.__receiver = StationManager(c*bs*k*m)
+        self.__link_tx = np.empty(self.__c*self.__bs*self.__k)
+        self.__link_rx = np.empty(self.__c*self.__bs*self.__k)
+        self.__coupling_loss = np.empty(self.__c*self.__bs*self.__k)
+        self.__acir = np.empty(self.__c*self.__bs*self.__k)
+        self.__thermal_noise = np.empty(self.__c*self.__bs*self.__k)
+        self.__bandwidth = ParametersImt.bandwidth
+        self.__frequency = ParametersImt.frequency        
+        
     def initialize(self, *args, **kwargs):
-        #self.configure_parameters()
-        if Parameters.static_bs:
-            self.create_static_bs()
+        self.__transmitter = \
+            StationManager.generate_imt_base_stations(self.__c*self.__bs, self.__topology)
     
     def snapshot(self, *args, **kwargs):
-        if not Parameters.static_bs:
-            self.create_random_bs()
-        self.create_other_station()
-        if Parameters.imt_interfered_with:
-            self.create_ue()
-            self.calculate_path_loss()
+        if not ParametersImt.static_base_stations:
+            # TODO: include support for randomly located base stations by
+            # creating the RandomTopology(Topology) class
+            pass
+        if ParametersImt.interfered_with:
+            #self.create_ue()
+            self.__receiver = StationManager.generate_imt_ue(self.c*self.bs*self.__k*self.__m,
+                                                             self.__topology.get_x_limits(),
+                                                             self.__topology.get_y_limits())
+            self.calculate_coupling_loss()
             self.connect_ue_to_bs()
-            self.apply_power_control()
-            self.scheduler()
+            #self.apply_power_control()
+            #self.scheduler()
             self.calculate_powers()
             self.add_external_interference()
             self.recalculate_sinr()
             self.calculate_imt_degradation()
         else:
-            #include beamforming
-            self.apply_power_control()
+            #self.beamforming()
+            #self.apply_power_control()
             #self.scheduler()
-            self.select_active_bs()
-            self.calculate_other_interference()
-            self.calculate_other_degradation()
-        self.collect_results()
+            #self.select_active_bs()
+            #self.calculate_other_interference()
+            #self.calculate_other_degradation()
+            pass
+        #self.collect_results()
 
     def finalize(self, *args, **kwargs):
         pass
-    
-    def create_static_bs(self):
-        D = Parameters.intersite_distance
-        h = D*math.sqrt(3)/2
-        
-        self.imt_base_stations.x = numpy.array( [ 0, D, D/2, -D/2, -D, -D/2, 
-                D/2, 2*D, 3*D/2, D, 0, -D, -3*D/2, -2*D, -3*D/2, -D, 0, D, 3*D/2 ] )
-        self.imt_base_stations.y = numpy.array( [ 0, 0, h, h, 0, -h, -h,
-                0, h, 2*h, 2*h, 2*h, h, 0, -h, -2*h, -2*h, -2*h, -h ] )
-        
-    def create_ue(self):
-        pass
-        
-    def select_active_bs(self):
-        self.imt_base_stations.active = numpy.random.uniform(size=19) < Parameters.bs_load_probability
-    
-    def apply_power_control( self ):
-        self.imt_base_stations.transmit_power = Parameters.bs_transmit_power
-        
-    def create_other_station( self ):
-        x_min = numpy.min(self.imt_base_stations.x)
-        x_max = numpy.max(self.imt_base_stations.x)
-        y_min = numpy.min(self.imt_base_stations.y)
-        y_max = numpy.max(self.imt_base_stations.y)
-                
-        self.satellite_receiver.x = ( x_max - x_min ) * numpy.random.random() + x_min
-        self.satellite_receiver.y = ( y_max - y_min ) * numpy.random.random() + y_min
-        
         
