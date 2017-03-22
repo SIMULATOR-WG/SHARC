0cf8c67343572c20c55fcba7e0fcc0546dbea983
edgar <edgar@anatel.gov.br>
2017-03-22 19:48:35 -0300

    Adding new instance attributes and properties. New methods: _select_ue, _scheduler, _apply_power_control, _calculate_sinr.

--- src/simulation_downlink.py
+++ src/simulation_downlink.py
@@ -6,6 +6,7 @@ Created on Wed Jan 11 19:06:41 2017
 """
 
 import numpy as np
+import math
 
 from simulation import Simulation
 from parameters.parameters_imt import ParametersImt
@@ -19,48 +20,47 @@ class SimulationDownlink(Simulation):
     Implements the flowchart of simulation downlink method
     """
     
-    def __init__(self):
+    def __init__(self, param: ParametersImt):
         np.random.seed(0)
         
-        self.__bs = ParametersImt.num_base_stations
-        self.__c = ParametersImt.num_clusters
-        self.__k = ParametersImt.ue_k
-        self.__m = ParametersImt.ue_k_m
+        self.__param = param
         
-        #self.__topology = TopologyMacrocell(ParametersImt.intersite_distance, ParametersImt.num_clusters)
-        self.__topology = TopologySingleBaseStation(ParametersImt.intersite_distance/2)
+        #self.__topology = TopologyMacrocell(self.__param.intersite_distance, self.__param.num_clusters)
+        self.__topology = TopologySingleBaseStation(self.__param.intersite_distance/2, 
+                                                    self.__param.num_clusters)
         self.__propagation = PropagationFreeSpace()
-
-        self.__coupling_loss = np.empty([self.__c*self.__bs, self.__k*self.__m])
-        self.__transmitter = \
-            StationManager.generate_imt_base_stations(self.__c*self.__bs, self.__topology)
-        self.__bandwidth = ParametersImt.bandwidth
-        self.__frequency = ParametersImt.frequency      
-      
+        
+        self.__num_transmitters = self.__param.num_clusters*self.__param.num_base_stations
+        self.__num_receivers = self.__param.num_clusters*self.__param.num_base_stations \
+                                 *self.__param.ue_k*self.__param.ue_k_m
+        
+        self.__coupling_loss = np.empty([self.__num_transmitters, self.__num_receivers])
+        self.__transmitter = np.empty(self.__num_transmitters)
+        self.__receiver = np.empty(self.__num_receivers)            
+        self.__link = dict([(bs,list()) for bs in range(self.__num_transmitters)])
         
     def initialize(self, *args, **kwargs):
         pass
-#        self.__transmitter = \
-#            StationManager.generate_imt_base_stations(self.__c*self.__bs, self.__topology)
+        self.__transmitter = \
+            StationManager.generate_imt_base_stations(self.__param,
+                                                      self.__topology)
     
     def snapshot(self, *args, **kwargs):
-        if not ParametersImt.static_base_stations:
+        if not self.__param.static_base_stations:
             # TODO: include support for randomly located base stations by
             # creating the RandomTopology(Topology) class
             pass
-        if ParametersImt.interfered_with:
-            #self.create_ue()
-            self.__receiver = StationManager.generate_imt_ue(self.__c*self.__bs*self.__k*self.__m,
-                                                             self.__topology.get_x_limits(),
-                                                             self.__topology.get_y_limits())
+        if self.__param.interfered_with:
+            self._create_ue()
             self._calculate_coupling_loss()
             self._connect_ue_to_bs()
-            #self._apply_power_control()
-            #self._scheduler()
-            self._calculate_powers()
-            self._add_external_interference()
-            self._recalculate_sinr()
-            self._calculate_imt_degradation()
+            self._select_ue()
+            self._scheduler()
+            self._apply_power_control()
+            self._calculate_sinr()
+            #self._add_external_interference()
+            #self._recalculate_sinr()
+            #self._calculate_imt_degradation()
         else:
             #self.beamforming()
             #self.apply_power_control()
@@ -74,26 +74,105 @@ class SimulationDownlink(Simulation):
     def finalize(self, *args, **kwargs):
         pass
         
+    def _create_ue(self):
+        self.__receiver = StationManager.generate_imt_ue(self.__param,
+                                                         self.__topology)    
+    
     def _calculate_coupling_loss(self):
         # Calculate distance from transmitters to receivers. The result is a
         # num_tx x num_rx array 
         d = self.__transmitter.get_distance_to(self.__receiver)
-        path_loss = self.__propagation.get_loss(distance=d, frequency=self.__frequency)
-        tx_antenna = self.__transmitter.get_tx_antenna()
-        rx_antenna = self.__receiver.get_rx_antenna()
+        path_loss = self.__propagation.get_loss(distance=d, frequency=self.__param.frequency)
+        tx_antenna = self.__transmitter.tx_antenna.astype('float')
+        rx_antenna = self.__receiver.rx_antenna.astype('float')
         # replicate columns to have the appropriate size
-        tx_gain = np.tile(tx_antenna, (self.__transmitter.get_num_stations(), self.__k))
-        rx_gain = np.tile(rx_antenna, (self.__transmitter.get_num_stations(), 1))
+        tx_gain = np.transpose(np.tile(tx_antenna, (self.__receiver.num_stations, 1)))
+        rx_gain = np.tile(rx_antenna, (self.__transmitter.num_stations, 1))
         # calculate coupling loss
-        self.__coupling_loss = np.maximum(tx_gain + path_loss + rx_gain, 
+        self.__coupling_loss = np.maximum(path_loss - tx_gain - rx_gain, 
                                           ParametersImt.mcl)
         
     def _connect_ue_to_bs(self):
         """
         Link the UE randomly to a BS to which the path coupling loss is within 
-        the smallest coupling loss plus the HO margin.
+        the minimum coupling loss plus the HO margin.
         """
-        for i in range(self.__receiver.get_num_stations()):
+        for ue in range(self.__num_receivers):
             # for each receiver, select the base stations 
-            bs_all = np.where(self.__coupling_loss[:,i] > ParametersImt.mcl + ParametersImt.ho_margin)[0]
-            bs_chosen = np.random.choice(bs_all)
+            bs_all = np.where(self.__coupling_loss[:,ue] < self.__param.mcl + self.__param.ho_margin)[0]
+            bs = np.random.choice(bs_all)
+            self.__link[bs].append(ue)
+            
+    def _select_ue(self):
+        """
+        Select K UEs randomly from all the UEs linked to one BS as “chosen” 
+        UEs. These K “chosen” UEs will be scheduled during this snapshot.
+        """
+        # TO BE IMPLEMENTED
+        pass
+        
+    def _scheduler(self):
+        for bs in range(self.__num_transmitters):
+            ue = self.link[bs]
+            bandwidth = self.__param.bandwidth / len(ue)
+            self.receiver.bandwidth[ue] = bandwidth
+    
+    def _apply_power_control(self):
+        """
+        Apply downlink power control algorithm
+        """
+        # Currently, the maximum transmit power of the base station is equaly
+        # divided among the selected UEs
+        p_max = math.pow(10, 0.1*self.__param.bs_tx_power)
+        # calculate tansmit powers to have a structure such as 
+        # {bs_1: [pwr_1, pwr_2,...], ...}, where bs_1 is the base station id, 
+        # pwr_1 is the transmit power from bs_1 to ue_1, pwr_2 is the transmit
+        # power from bs_1 to ue_2, etc
+        self.transmitter.tx_power = dict([(bs,[10*math.log10(p_max/len(self.link[bs]))] * len(self.link[bs])) for bs in range(self.__num_transmitters)])
+        
+    def _calculate_sinr(self):
+        bs_all = [b for b in range(self.__num_transmitters)]
+        
+        # calculate received power for each UE
+        for bs, ue_list in self.link.items():
+            self.receiver.rx_power[ue_list] = np.array(self.transmitter.tx_power[bs]) \
+                                    - np.array(self.coupling_loss[bs,ue_list])
+            # create a list with base stations that generate interference in ue_list
+            bs_interf = [b for b in bs_all if b not in [bs]]
+
+            # calculate intra system interference
+            for ue in ue_list:
+                for bi in bs_interf:
+                    interference = self.transmitter.tx_power[bi] \
+                        - self.coupling_loss[bi,ue]
+                    self.receiver.rx_interference[ue] = 10*math.log10( \
+                          math.pow(10, 0.1*self.receiver.rx_interference[ue]) \
+                        + np.sum(np.power(10, 0.1*interference)))
+        
+        self.receiver.thermal_noise = \
+            10*math.log10(self.__param.BOLTZMANN_CONSTANT*self.__param.noise_temperature) + \
+            10*np.log10(self.receiver.bandwidth * 1e6) + \
+            self.receiver.noise_figure
+            
+        self.receiver.total_interference = \
+            10*np.log10(np.power(10, 0.1*self.receiver.rx_interference) + \
+                        np.power(10, 0.1*self.receiver.thermal_noise))
+        self.receiver.sinr = self.receiver.rx_power - self.receiver.total_interference                    
+                    
+                    
+    @property
+    def transmitter(self):
+        return self.__transmitter
+        
+    @property
+    def receiver(self):
+        return self.__receiver
+        
+    @property
+    def coupling_loss(self):
+        return self.__coupling_loss
+        
+    @property
+    def link(self):
+        return self.__link
+    
