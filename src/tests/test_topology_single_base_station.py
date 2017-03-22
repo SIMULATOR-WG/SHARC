1291ca12d58c967d0bc040479ddf49dd130ad42a
edgar <edgar@anatel.gov.br>
2017-03-22 19:45:25 -0300

    Updating tests to support 2 clusters

--- src/tests/test_topology_single_base_station.py
+++ src/tests/test_topology_single_base_station.py
@@ -13,21 +13,32 @@ class TopologySingleBaseStationTest(unittest.TestCase):
     
     def setUp(self):
         cell_radius = 1500
-        self.topology = TopologySingleBaseStation(cell_radius)
-        
-    def test_cell_radius(self):
-        # check initial value
-        self.assertEqual(self.topology.cell_radius, 1500)
-        # change cell radius and check it
-        self.topology.cell_radius = 700
-        self.assertEqual(self.topology.cell_radius, 700)
-
-        self.topology.cell_radius = 400
-        self.assertEqual(self.topology.cell_radius, 400)
+        num_clusters = 1
+        self.topology = TopologySingleBaseStation(cell_radius, num_clusters)
         
     def test_coordinates(self):
-        self.assertEqual(self.topology.x, 0)
-        self.assertEqual(self.topology.y, 0)
+        self.assertEqual(self.topology.x, [0])
+        self.assertEqual(self.topology.y, [0])
+        # with a single cluster there is no change in coordinates
+        self.topology.intersite_distance = 1000
+        self.assertEqual(self.topology.x, [0])
+        self.assertEqual(self.topology.y, [0])
+        self.topology.cell_radius = 1000
+        self.assertEqual(self.topology.x, [0])
+        self.assertEqual(self.topology.y, [0])
+        
+        # now we change to 2 clusters
+        self.topology.cell_radius = 1500
+        self.topology.num_clusters = 2
+        self.assertEqual(self.topology.x, [-1500, 1500])
+        self.assertEqual(self.topology.y, [0, 0])
+        # 
+        self.topology.intersite_distance = 1000
+        self.assertEqual(self.topology.x, [-500, 500])
+        self.assertEqual(self.topology.y, [0, 0])
+        self.topology.cell_radius = 500
+        self.assertEqual(self.topology.x, [-500, 500])
+        self.assertEqual(self.topology.y, [0, 0])
         
     def test_limits(self):
         # check initial values
@@ -37,27 +48,54 @@ class TopologySingleBaseStationTest(unittest.TestCase):
         self.assertEqual(self.topology.y_max,  1500)
         # change cell radius and check it
         self.topology.cell_radius = 1000
-        
         self.assertEqual(self.topology.x_min, -1000)
         self.assertEqual(self.topology.x_max,  1000)
         self.assertEqual(self.topology.y_min, -1000)
         self.assertEqual(self.topology.y_max,  1000)
         
+        # change to 2 clusters
+        self.topology.cell_radius = 1500
+        self.topology.num_clusters = 2        
+        self.assertEqual(self.topology.x_min, -3000)
+        self.assertEqual(self.topology.x_max,  3000)
+        self.assertEqual(self.topology.y_min, -1500)
+        self.assertEqual(self.topology.y_max,  1500)
+        # change cell radius and check it
+        self.topology.cell_radius = 1000
+        self.assertEqual(self.topology.x_min, -2000)
+        self.assertEqual(self.topology.x_max,  2000)
+        self.assertEqual(self.topology.y_min, -1000)
+        self.assertEqual(self.topology.y_max,  1000)        
+        
     def test_num_clusters(self):
-        with self.assertRaises(NotImplementedError):
-            self.topology.num_clusters = 1
-        with self.assertRaises(NotImplementedError):
-            self.topology.num_clusters = 2            
-        with self.assertRaises(NotImplementedError):
-            self.topology.num_clusters = 7
+        self.assertEqual(self.topology.num_clusters, 1)
+        self.topology.num_clusters = 2
+        self.assertEqual(self.topology.num_clusters, 2)
+        with self.assertRaises(ValueError):
+            self.topology.num_clusters = 3
         
     def test_intersite_distance(self):
-        with self.assertRaises(NotImplementedError):
-            self.topology.intersite_distance = 100
-        with self.assertRaises(NotImplementedError):
-            self.topology.intersite_distance = 500        
-        with self.assertRaises(NotImplementedError):
-            self.topology.intersite_distance = 1000  
+        self.assertEqual(self.topology.intersite_distance, 3000)
+        
+        self.topology.intersite_distance = 1000
+        self.assertEqual(self.topology.intersite_distance, 1000)
+        self.assertEqual(self.topology.cell_radius, 500)
+        
+        self.topology.intersite_distance = 2000
+        self.assertEqual(self.topology.intersite_distance, 2000)
+        self.assertEqual(self.topology.cell_radius, 1000)      
+        
+    def test_cell_radius(self):
+        # check initial value
+        self.assertEqual(self.topology.cell_radius, 1500)
+        # change cell radius and check it
+        self.topology.cell_radius = 1000
+        self.assertEqual(self.topology.intersite_distance, 2000)
+        self.assertEqual(self.topology.cell_radius, 1000)
+        # change cell radius and check it
+        self.topology.cell_radius = 2000
+        self.assertEqual(self.topology.intersite_distance, 4000)
+        self.assertEqual(self.topology.cell_radius, 2000)         
             
 if __name__ == '__main__':
     unittest.main()        
