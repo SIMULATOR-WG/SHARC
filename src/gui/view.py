f08ce835735f96647f15bbb66b4d4f43e791171e
edgar <edgar@anatel.gov.br>
2017-03-23 15:31:59 -0300

    Adding new feature to plot results

--- src/gui/view.py
+++ src/gui/view.py
@@ -9,7 +9,10 @@ from controller import Controller
 from support.observer import Observer
 from support.enumerations import Action, State
 from gui.thread_safe_scrolled_text import ThreadSafeScrolledText
+from results import Results
+import matplotlib.pyplot as plt
 
+import queue
 import logging
 import tkinter
 import tkinter.scrolledtext
@@ -24,6 +27,8 @@ class View(tkinter.Tk, Observer):
         super(View, self).__init__()
         self.parent = parent
         self.initialize()
+        self.__results_queue = queue.Queue()
+        self.__results = None
         
     def initialize(self):
         """
@@ -39,33 +44,40 @@ class View(tkinter.Tk, Observer):
 
         self.__scrolledtext = ThreadSafeScrolledText(self.__frame, 
             wrap=tkinter.WORD, width=80, height=25, bd=5)
-        self.__scrolledtext.grid(column=0, row=1, columnspan=6, sticky='EW')
+        self.__scrolledtext.grid(column=0, row=1, columnspan=7, sticky='EW')
 
         self.__start_image = tkinter.PhotoImage(file = "img/start_icon.png")
-        self.__start_button = tkinter.Button(self.__frame, text=" START",  
+        self.__start_button = tkinter.Button(self.__frame, text="START",  
             image=self.__start_image, compound=tkinter.LEFT, 
             state=tkinter.NORMAL, command=self.__on_start_button_click)
         self.__start_button.bind("<Return>", self.__on_start_button_click)
         self.__start_button.grid(column=1, row=0, sticky='EW')
         
         self.__stop_image = tkinter.PhotoImage(file="img/stop_icon.png")
-        self.__stop_button = tkinter.Button(self.__frame, text=" STOP  ",  
+        self.__stop_button = tkinter.Button(self.__frame, text="STOP  ",  
             image=self.__stop_image, compound=tkinter.LEFT, 
             state=tkinter.DISABLED, command=self.__on_stop_button_click)
         self.__stop_button.bind("<Return>", self.__on_stop_button_click)
         self.__stop_button.grid(column=2, row=0, sticky='EW')
         
+        self.__results_image = tkinter.PhotoImage(file = "img/results_icon.png")
+        self.__results_button = tkinter.Button(self.__frame, text="RESULTS",  
+            image=self.__results_image, compound=tkinter.LEFT, 
+            state=tkinter.DISABLED, command=self.__on_results_button_click)
+        self.__results_button.bind("<Return>", self.__on_results_button_click)
+        self.__results_button.grid(column=3, row=0, sticky='EW')        
+        
         self.__clear_image = tkinter.PhotoImage(file="img/clear_icon.png")
         self.__clear_button = tkinter.Button(self.__frame, text="CLEAR",  
             image=self.__clear_image, compound=tkinter.LEFT, 
             state=tkinter.NORMAL, command=self.__on_clear_button_click)
-        self.__clear_button.grid(column=3, row=0, sticky='EW')          
+        self.__clear_button.grid(column=4, row=0, sticky='EW')          
         
         self.__copy_image = tkinter.PhotoImage(file="img/copy_icon.png")
         self.__copy_button = tkinter.Button(self.__frame, text="COPY",  
             image=self.__copy_image, compound=tkinter.LEFT, 
             state=tkinter.NORMAL, command=self.__on_copy_button_click)
-        self.__copy_button.grid(column=4, row=0, sticky='EW')        
+        self.__copy_button.grid(column=5, row=0, sticky='EW')        
         
         self.grid_columnconfigure(0, weight=1)
         self.resizable(False, False)
@@ -89,6 +101,18 @@ class View(tkinter.Tk, Observer):
         self.__controller.action(Action.STOP_SIMULATION)
         self.__set_state(State.STOPPING)
         
+    def __on_results_button_click(self, *args):
+        """
+        This method is called when results button is clicked
+        """
+        if not self.__results:
+            try:
+                self.__results = self.__results_queue.get_nowait()
+            except queue.Empty:
+                pass
+        else:
+            self.__plot_results(self.__results)
+
     def __on_clear_button_click(self):
         """
         This method is called when clear button is clicked
@@ -102,6 +126,55 @@ class View(tkinter.Tk, Observer):
         self.clipboard_clear()
         self.clipboard_append(self.__scrolledtext.get(1.0, tkinter.END))  
         self.__popup("Copied to clipboard.")
+        
+    def __plot_results(self, results: Results):
+        n_bins = 500
+        
+        #plt.figure(figsize=(16,10), facecolor='w', edgecolor='k')
+        #plt.subplot(2, 2, 1)
+        plt.figure(figsize=(8,6), facecolor='w', edgecolor='k')
+        plt.hist(results.coupling_loss_dl, n_bins, normed=True, 
+                 histtype='step', cumulative=True )
+        plt.ylim((0, 1))
+        plt.title("CDF of IMT coupling loss [dB]")
+        plt.xlabel("IMT coupling loss [dB]")
+        plt.ylabel("Probability of IMT coupling loss < $X$")
+        plt.grid()
+        
+        #plt.subplot(2, 2, 3)
+        plt.figure(figsize=(8,6), facecolor='w', edgecolor='k')
+        plt.hist(results.tx_power_dl, n_bins, normed=True, 
+                 histtype='step', cumulative=True )
+        plt.ylim((0, 1))
+        plt.title("CDF of base station transmit power [dB]")
+        plt.xlabel("Base station transmit power [dBm]")
+        plt.ylabel("Probability of transmit power < $X$")
+        plt.grid()        
+        
+        #plt.subplot(2, 2, 2)
+        plt.figure(figsize=(8,6), facecolor='w', edgecolor='k')
+        plt.hist(results.snr_dl, n_bins, normed=True, 
+                 histtype='step', cumulative=True )
+        plt.ylim((0, 1))
+        plt.title("CDF of $C/N$ [dB]")
+        plt.xlabel("$C/N$ [dB]")
+        plt.ylabel("Probability $C/N < X$")
+        plt.grid()
+
+        #plt.subplot(2, 2, 4)
+        plt.figure(figsize=(8,6), facecolor='w', edgecolor='k')
+        plt.hist(results.sinr_dl, n_bins, normed=True, 
+                 histtype='step', cumulative=True )
+        plt.ylim((0, 1))
+        plt.title("CDF of $C/(N+I)$ [dB]")
+        plt.xlabel("$C/(N+I)$ [dB]")
+        plt.ylabel("Probability $C/(N+I) < X$")
+        plt.grid()        
+        
+        plt.tight_layout()
+        
+        #plt.figure(1).patch.set_facecolor("white")        
+        plt.show()
         
     def __insert_text(self, source: str, text: str):
         """
@@ -128,20 +201,28 @@ class View(tkinter.Tk, Observer):
         ----------
             state : Enumaration that defines the simulation state
         """
-        if state == State.INITIAL or state == State.FINISHED or state == State.STOPPED:
+        if state == State.INITIAL:
             self.__start_button.config(state=tkinter.NORMAL)
             self.__stop_button.config(state=tkinter.DISABLED)
+            self.__results_button.config(state=tkinter.DISABLED)
             self.__start_button.focus_set()
         elif state == State.RUNNING:
             self.__start_button.config(state=tkinter.DISABLED)
             self.__stop_button.config(state=tkinter.NORMAL)
+            self.__results_button.config(state=tkinter.DISABLED)
             self.__stop_button.focus_set()
         elif state == State.STOPPING:
             self.__start_button.config(state=tkinter.DISABLED)
             self.__stop_button.config(state=tkinter.DISABLED)
+            self.__results_button.config(state=tkinter.DISABLED)
+        elif state == State.FINISHED or state == State.STOPPED:
+            self.__start_button.config(state=tkinter.NORMAL)
+            self.__stop_button.config(state=tkinter.DISABLED)
+            self.__results_button.config(state=tkinter.NORMAL)            
         else:
             self.__start_button.config(state=tkinter.NORMAL)
             self.__stop_button.config(state=tkinter.DISABLED)
+            self.__results_button.config(state=tkinter.DISABLED)
             self.__start_button.focus_set()
             
     def set_controller(self, controller: Controller):
@@ -169,7 +250,12 @@ class View(tkinter.Tk, Observer):
             #self.insert_text( __name__, "\n" )
         if "message" in kwargs:
             self.__insert_text(kwargs["source"], kwargs["message"])
+        if "results" in kwargs:
+            self.__update_results_queue(kwargs["results"])
         
+    def __update_results_queue(self, results: Results):
+        self.__results_queue.put(results)        
+            
     def __popup(self, message: str):
         """
         Displays a message on a popup window.
