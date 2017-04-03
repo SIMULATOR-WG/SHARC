de7f1beb4478789c7ebf0803af9ca295acf99a57
edgar <edgar@anatel.gov.br>
2017-04-03 19:12:44 -0300

    Changing the UE generation routine to ensure that K UE's will be generated on the area around a BS

--- src/station_factory.py
+++ src/station_factory.py
@@ -36,8 +36,22 @@ class StationFactory(object):
     def generate_imt_ue(param: ParametersImt, topology: Topology):
         num_ue = param.num_clusters*param.num_base_stations*param.ue_k*param.ue_k_m
         imt_ue = StationManager(num_ue)
-        imt_ue.x = (topology.x_max - topology.x_min)*np.random.random(num_ue) + topology.x_min
-        imt_ue.y = (topology.y_max - topology.y_min)*np.random.random(num_ue) + topology.y_min
+        #imt_ue.x = (topology.x_max - topology.x_min)*np.random.random(num_ue) + topology.x_min
+        #imt_ue.y = (topology.y_max - topology.y_min)*np.random.random(num_ue) + topology.y_min
+        ue_x = list()
+        ue_y = list()
+        for bs in range(topology.x.size):
+            x_min = topology.x[bs] - topology.cell_radius
+            x_max = topology.x[bs] + topology.cell_radius
+            y_min = topology.y[bs] - topology.cell_radius
+            y_max = topology.y[bs] + topology.cell_radius
+            x = (x_max - x_min)*np.random.random(param.ue_k*param.ue_k_m) + x_min
+            y = (y_max - y_min)*np.random.random(param.ue_k*param.ue_k_m) + y_min
+            ue_x.extend(x)
+            ue_y.extend(y)
+        imt_ue.x = np.array(ue_x)
+        imt_ue.y = np.array(ue_y)   
+        imt_ue.active = np.zeros(num_ue, dtype=bool)
         imt_ue.height = param.ue_height*np.ones(num_ue)
         imt_ue.tx_power = param.ue_tx_power*np.ones(num_ue)
         imt_ue.rx_interference = -300*np.ones(num_ue)
