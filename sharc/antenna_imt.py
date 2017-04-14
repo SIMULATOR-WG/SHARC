926ed56828733ab331fef798aeac394f168b949e
CalilQ <calil_queiroz@hotmail.com>
2017-04-14 17:36:55 -0300

    Implement sector antenna main methods

--- sharc/antenna_imt.py
+++ sharc/antenna_imt.py
@@ -21,7 +21,7 @@ class AntennaImt(Antenna):
         sla_v (float): element vertical sidelobe attenuation
     """
     
-    def __init__(self,g_max: float, theta_3db: float, phy_3db: float, am: float, sla_v: float):
+    def __init__(self,g_max: float, phy_3db: float, theta_3db: float, am: float, sla_v: float):
         """
         Constructs an AntennaImt object.
         
@@ -34,8 +34,8 @@ class AntennaImt(Antenna):
             sla_v (float): vertical front-to-back ratio
         """
         self.__g_max = g_max
-        self.__theta_3db = theta_3db
         self.__phy_3db = phy_3db
+        self.__theta_3db = theta_3db
         self.__am = am
         self.__sla_v = sla_v
         
@@ -46,13 +46,14 @@ class AntennaImt(Antenna):
     def g_max(self):
         return self.__g_max
     
-    @property
-    def theta_3db(self):
-        return self.__theta_3db
-    
+        
     @property
     def phy_3db(self):
         return self.__phy_3db
+    
+    @property
+    def theta_3db(self):
+        return self.__theta_3db
     
     @property
     def am(self):
@@ -61,20 +62,6 @@ class AntennaImt(Antenna):
     @property
     def sla_v(self):
         return self.__sla_v
-    
-    def vertical_pattern(self,theta: float) -> float:
-        """
-        Calculates the vertical radiation pattern.
-        
-        Parameters
-        ----------
-            theta (float): elevation angle [degrees]
-            
-        Returns
-        -------
-            a_v (float): vertical radiation pattern gain value
-        """
-        pass
     
     def horizontal_pattern(self,phy: float) -> float:
         """
@@ -88,9 +75,23 @@ class AntennaImt(Antenna):
         -------
             a_h (float): horizontal radiation pattern gain value
         """
-        pass
+        return -1.0*min(12*(phy/self.phy_3db)**2,self.am)
+    
+    def vertical_pattern(self,theta: float) -> float:
+        """
+        Calculates the vertical radiation pattern.
         
-    def element_pattern(self, theta: float, phy: float) -> float:
+        Parameters
+        ----------
+            theta (float): elevation angle [degrees]
+            
+        Returns
+        -------
+            a_v (float): vertical radiation pattern gain value
+        """
+        return -1.0*min(12*((theta-90.0)/self.theta_3db)**2,self.sla_v)
+        
+    def element_pattern(self, phy: float, theta: float) -> float:
         """
         Calculates the element radiation pattern gain.
         
@@ -101,6 +102,10 @@ class AntennaImt(Antenna):
             
         Returns
         -------
-            a_e (float): element radiation pattern gain value
+            gain (float): element radiation pattern gain value
         """
-        pass
+        att = -1.0*(self.horizontal_pattern(phy) + \
+                    self.vertical_pattern(theta))
+        self.gain = self.g_max - min(att,self.am)
+        
+        return self.gain
--- tests/test_antenna_imt.py
+++ tests/test_antenna_imt.py
@@ -14,13 +14,15 @@ class AntennaImtTest(unittest.TestCase):
     def setUp(self):
         #Element parameters
         g_max = 5
-        theta_3db = 60
         phy_3db = 80
+        theta_3db = 60
         am = 30
         sla_v = 30
         
-        # Create antenna object
-        self.antenna1 = AntennaImt(g_max,theta_3db,phy_3db,am,sla_v)
+        # Create antenna IMT objects
+        self.antenna1 = AntennaImt(g_max,phy_3db,theta_3db,am,sla_v)
+        self.antenna2 = AntennaImt(g_max,phy_3db,theta_3db,am,sla_v)
+        self.antenna2.gain = 15
         
     def test_gain(self):
         self.assertEqual(self.antenna1.gain,0)
@@ -45,59 +47,50 @@ class AntennaImtTest(unittest.TestCase):
     
     def test_radd(self):
         self.assertEqual(2 + self.antenna1, 2)
+        self.assertEqual(self.antenna1 + self.antenna2, 15)
         
     def test_sub(self):
         self.assertEqual(self.antenna1 - 1, -1)
     
     def test_rsub(self):
         self.assertEqual(9 - self.antenna1, 9)
+        self.assertEqual(self.antenna2 - self.antenna1, 15)
      
     def test_lt(self):
         self.assertTrue(self.antenna1 < 5)
         self.assertFalse(self.antenna1 < -3)
+        self.assertTrue(self.antenna1 < self.antenna2)
         
     def test_le(self):
         self.assertTrue(self.antenna1 <= 5)
         self.assertTrue(self.antenna1 <= 0)
         self.assertFalse(self.antenna1 <= -2)
+        self.assertTrue(self.antenna1 <= self.antenna2)
 
     def test_gt(self):
         self.assertFalse(self.antenna1 > 5)
         self.assertTrue(self.antenna1 > -2)
+        self.assertFalse(self.antenna1 > self.antenna2)
         
     def test_ge(self):
         self.assertTrue(self.antenna1 >= 0)
         self.assertTrue(self.antenna1 >= -3)
         self.assertFalse(self.antenna1 >= 3)
+        self.assertFalse(self.antenna1 >= self.antenna2)
         
     def test_eq(self):
         self.assertTrue(self.antenna1 == 0)
         self.assertFalse(self.antenna1 == 7)
+        self.assertFalse(self.antenna1 == self.antenna2)
         
     def test_ne(self):
         self.assertTrue(self.antenna1 != 1)
         self.assertFalse(self.antenna1 != 0)
-        
-    def test_vertical_pattern(self):
-        # theta = 0 results in zero gain
-        theta = 0
-        v_att = self.antenna1.vertica_pattern(theta)
-        self.assertEqual(v_att,0.0)
-        
-        # theta = 180 implies vertical gain of -27 dB
-        theta = 180
-        v_att = self.antenna1.vertica_pattern(theta)
-        self.assertEqual(v_att,-27.0)
-        
-        # theta = 210, vertical attenuation equals vertical sidelobe attenuation
-        theta = 210
-        v_att = self.antenna1.vertica_pattern(theta)
-        self.assertEqual(v_att,-30)
-        self.assertEqual(v_att,-1.0*self.antenna1.sla_v)
+        self.assertTrue(self.antenna1 != self.antenna2)
         
     def test_horizontal_pattern(self):  
-        # phy = 90 results in zero gain
-        phy = 90
+        # phy = 0 results in zero gain
+        phy = 0
         h_att = self.antenna1.horizontal_pattern(phy)
         self.assertEqual(h_att,0.0)
         
@@ -112,26 +105,43 @@ class AntennaImtTest(unittest.TestCase):
         self.assertEqual(h_att,-30)
         self.assertEqual(h_att,-1.0*self.antenna1.am)
         
+    def test_vertical_pattern(self):
+        # theta = 90 results in zero gain
+        theta = 90
+        v_att = self.antenna1.vertical_pattern(theta)
+        self.assertEqual(v_att,0.0)
+        
+        # theta = 180 implies vertical gain of -27 dB
+        theta = 180
+        v_att = self.antenna1.vertical_pattern(theta)
+        self.assertEqual(v_att,-27.0)
+        
+        # theta = 210, vertical attenuation equals vertical sidelobe attenuation
+        theta = 210
+        v_att = self.antenna1.vertical_pattern(theta)
+        self.assertEqual(v_att,-30)
+        self.assertEqual(v_att,-1.0*self.antenna1.sla_v)
+        
     def test_element_pattern(self):
         # theta = 0 and phy = 90 result in maximum gain
-        theta = 0
-        phy = 90
-        e_gain = self.antenna1.element_pattern(theta,phy)
+        phy = 0
+        theta = 90
+        e_gain = self.antenna1.element_pattern(phy,theta)
         self.assertEqual(e_gain,5.0)
         self.assertEqual(e_gain,self.antenna1.g_max)
         
         # Check for gain update
         self.assertTrue(self.antenna1 == 5.0)
         
-        theta = 150
         phy = 80
-        e_gain = self.antenna1.element_pattern(theta,phy)
+        theta = 150
+        e_gain = self.antenna1.element_pattern(phy,theta)
         self.assertEqual(e_gain,-19.0)
         self.assertTrue(self.antenna1 == -19.0)
         
-        theta = 210
         phy = 150
-        e_gain = self.antenna1.element_pattern(theta,phy)
+        theta = 210
+        e_gain = self.antenna1.element_pattern(phy,theta)
         self.assertEqual(e_gain,-25.0)
         self.assertEqual(e_gain,self.antenna1.g_max - self.antenna1.am)
         self.assertTrue(self.antenna1 == -25.0)
