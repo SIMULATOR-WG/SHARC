f9257e72dc0f340297eaa63d091c54984518b5c6
CalilQ <calil_queiroz@hotmail.com>
2017-04-17 16:12:45 -0300

    Fix antenna import in test

--- tests/test_antenna_beamforming_imt.py
+++ tests/test_antenna_beamforming_imt.py
@@ -9,7 +9,7 @@ import unittest
 import numpy as np
 
 from sharc.antenna.antenna_beamforming_imt import AntennaBeamformingImt
-from sharc.antenna.parameters.parameters_antenna_imt import ParametersAntennaImt
+from sharc.parameters.parameters_antenna_imt import ParametersAntennaImt
 
 class AntennaBeamformingImtTest(unittest.TestCase):
     
