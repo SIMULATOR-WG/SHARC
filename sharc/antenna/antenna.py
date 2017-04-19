6c7debbaf80d3db1d1e15783646693d28e9bb054
CalilQ <calil_queiroz@hotmail.com>
2017-04-19 16:56:05 -0300

    Implement sector antenna

--- sharc/antenna/antenna.py
+++ sharc/antenna/antenna.py
@@ -12,7 +12,6 @@ class Antenna(object):
     
     def __init__(self, gain: float = 0):
         self.gain = gain
-    
         
     """
     TODO: check the validity of operator overriding for Antenna class because
