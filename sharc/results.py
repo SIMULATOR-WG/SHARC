6d1512302803e8eb907e0524bc51727158c8cbab
edgar <edgar@anatel.gov.br>
2017-05-12 16:14:52 -0300

    Results module is totally refactored. It uses the brand new Plot class and creates a list of plots. The write_files method can be called anytime during the simulation to write partial results to text files into the output directory.

--- sharc/results.py
+++ sharc/results.py
@@ -5,53 +5,172 @@ Created on Thu Mar 23 08:47:46 2017
 @author: edgar
 """
 
+from sharc.plot import Plot
+
 import numpy as np
 import os
 
 class Results(object):
     
     def __init__(self):
-        self.tx_power_ul = list()
-        self.interf_power_ul = list()
-        self.sinr_ul = list()
-        self.snr_ul = list()
-        self.throughoput_ul = list()
-        self.inr = list()
-        self.coupling_loss_ul = list()
-        self.coupling_loss_ue_sat = list()
-        self.coupling_loss_bs_sat = list()
+        self.output_directory = "output"
         
-    def add_coupling_loss_ul(self, sample):
-        self.coupling_loss_ul.extend(sample)
-        
-    def add_tx_power_ul(self, sample):
-        self.tx_power_ul.extend(sample)
+        self.imt_ul_tx_power_density = list()
+        self.imt_ul_sinr = list()
+        self.imt_ul_snr = list()
+        self.imt_ul_tput = list()
+        self.imt_ul_coupling_loss = list()
 
-    def add_interf_power_ul(self, sample):
-        self.interf_power_ul.extend(sample)
+        self.imt_dl_tx_power = list()
+        self.imt_dl_sinr = list()
+        self.imt_dl_snr = list()
+        self.imt_dl_tput = list()
+        self.imt_dl_coupling_loss = list()
         
-    def add_sinr_ul(self, sample):
-        self.sinr_ul.extend(sample)
+        self.system_ul_coupling_loss = list()
+        self.system_ul_interf_power = list()
+
+        self.system_dl_coupling_loss = list()
+        self.system_dl_interf_power = list()
+
+        self.system_inr = list()
         
-    def add_snr_ul(self, sample):
-        self.snr_ul.extend(sample)        
+        self.plot_list = list()
         
-    def add_inr(self, sample):
-        self.inr.extend(sample)
-        
-    def add_coupling_loss_ue_sat(self, sample):
-        self.coupling_loss_ue_sat.extend(sample)
-        
-    def add_coupling_loss_bs_sat(self, sample):
-        self.coupling_loss_bs_sat.extend(sample)
-        
-    def write_files(self):
-        #np.savetxt(os.path.join("results", "coupling_loss_ul.txt"), np.array(self.coupling_loss_ul))
-        
-        np.savetxt(os.path.join("output", "coupling_loss_ue_sat.txt"), 
-                   np.array(self.coupling_loss_ue_sat), fmt="%10.5f")
-        np.savetxt(os.path.join("output", "coupling_loss_bs_sat.txt"), 
-                   np.array(self.coupling_loss_bs_sat), fmt="%10.5f")
-        np.savetxt(os.path.join("output", "inr.txt"), 
-                   np.array(self.inr), fmt="%10.5f")
-        
+    def generate_plot_list(self, n_bins):
+        self.plot_list = list()
+        if len(self.imt_ul_tx_power_density) > 0:
+            values, base = np.histogram(self.imt_ul_tx_power_density, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            x_label = "UL transmit power density [dBm/Hz]"
+            y_label = "Probability of UL transmit power density < $X$"
+            title = "[IMT] CDF of UL transmit power density"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_ul_sinr) > 0:
+            values, base = np.histogram(self.imt_ul_sinr, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            x_label = "UL SINR [dB]"
+            y_label = "Probability of SINR < $X$"
+            title = "[IMT] CDF of UL SINR"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_ul_snr) > 0:
+            values, base = np.histogram(self.imt_ul_snr, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[IMT] CDF of UL SNR"
+            x_label = "UL SNR [dB]"
+            y_label = "Probability of SNR < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_ul_tput) > 0:
+            values, base = np.histogram(self.imt_ul_tput, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[IMT] CDF of UL throughput"
+            x_label = "UL throughput [bits/s/Hz]"
+            y_label = "Probability of UL throughput < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_ul_coupling_loss) > 0:
+            values, base = np.histogram(self.imt_ul_coupling_loss, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[IMT] CDF of UL coupling loss"
+            x_label = "UL coupling loss [dB]"
+            y_label = "Probability of UL coupling loss < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_dl_tx_power) > 0:
+            values, base = np.histogram(self.imt_dl_tx_power, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            x_label = "UL transmit power [dBm]"
+            y_label = "Probability of UL transmit power < $X$"
+            title = "[IMT] CDF of UL transmit power"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_dl_sinr) > 0:
+            values, base = np.histogram(self.imt_dl_sinr, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            x_label = "UL SINR [dB]"
+            y_label = "Probability of SINR < $X$"
+            title = "[IMT] CDF of UL SINR"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_dl_snr) > 0:
+            values, base = np.histogram(self.imt_dl_snr, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[IMT] CDF of UL SNR"
+            x_label = "UL SNR [dB]"
+            y_label = "Probability of SNR < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_dl_tput) > 0:
+            values, base = np.histogram(self.imt_dl_tput, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[IMT] CDF of UL throughput"
+            x_label = "UL throughput [bits/s/Hz]"
+            y_label = "Probability of UL throughput < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.imt_dl_coupling_loss) > 0:
+            values, base = np.histogram(self.imt_dl_coupling_loss, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[IMT] CDF of UL coupling loss"
+            x_label = "UL coupling loss [dB]"
+            y_label = "Probability of UL coupling loss < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))
+        if len(self.system_inr) > 0:
+            values, base = np.histogram(self.system_inr, bins=n_bins)
+            cumulative = np.cumsum(values)
+            x = base[:-1]
+            y = cumulative / cumulative[-1]
+            title = "[FSS] CDF of FSS INR"
+            x_label = "INR [dB]"
+            y_label = "Probability of INR < $X$"
+            file_name = title
+            y_limits = (0, 1)
+            self.plot_list.append(Plot(x, y, x_label, y_label, title, file_name, y_lim=y_limits))            
+            
+    def write_files(self, snapshot_number: int):
+        n_bins = 200
+        file_extension = ".txt"
+        header_text = "Results collected after " + str(snapshot_number) + " snapshots."
+        self.generate_plot_list(n_bins)
+            
+        for plot in self.plot_list:
+            np.savetxt(os.path.join(self.output_directory, plot.file_name + file_extension), 
+                       np.transpose([plot.x, plot.y]), 
+                       fmt="%.5f", delimiter="\t", header=header_text, 
+                       newline=os.linesep)
+
+      
