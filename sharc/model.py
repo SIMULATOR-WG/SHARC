72aa5e7c112ddb57ac8a8de54b305a44bfe84b2e
edgar <edgar@anatel.gov.br>
2017-05-12 16:09:38 -0300

    Renaming the method that performs the snapshot. Small adjustment on the snapshot count and passing it as a parameter to the finalize method.

--- sharc/model.py
+++ sharc/model.py
@@ -35,19 +35,22 @@ class Model(Observable):
         self.notify_observers(source=__name__,
                               message="Simulation is running...",
                               state=State.RUNNING )
-        self.current_snapshot = 1
+        self.current_snapshot = 0
         self.simulation.initialize()
         
-    def step(self):
+    def snapshot(self):
         """
         Performs one simulation step and collects the results
         """
+        write_to_file = False
+        self.current_snapshot += 1
+
         if not self.current_snapshot % 10:
+            write_to_file = True
             self.notify_observers(source=__name__,
                                   message="Snapshot #" + str(self.current_snapshot))
-        #time.sleep(1)
-        self.simulation.snapshot()
-        self.current_snapshot += 1
+
+        self.simulation.snapshot(write_to_file, self.current_snapshot)
             
     def is_finished(self) -> bool:
         """
@@ -58,7 +61,7 @@ class Model(Observable):
         -------
             True if simulation is finished; False otherwise.
         """
-        if self.current_snapshot <= ParametersGeneral.num_snapshots:
+        if self.current_snapshot < ParametersGeneral.num_snapshots:
             return False
         else:
             return True
@@ -67,7 +70,7 @@ class Model(Observable):
         """
         Finalizes the simulation and performs all post-simulation tasks
         """
-        self.simulation.finalize()
+        self.simulation.finalize(self.current_snapshot)
         self.notify_observers(source=__name__, 
                               message="FINISHED!", state=State.FINISHED)
         
