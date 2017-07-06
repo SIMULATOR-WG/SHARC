9c0a954f39ec33068bbe85d85803199d4d10fe0c
edgar <edgar@anatel.gov.br>
2017-07-06 20:51:28 -0300

    Refactoring all propagation classes.

--- sharc/propagation/propagation_sat_simple.py
+++ sharc/propagation/propagation_sat_simple.py
@@ -13,18 +13,23 @@ class PropagationSatSimple(Propagation):
     """
     Implements the simplified satellite propagation model
     """
-    def __init__(self, line_of_sight_prob: float = 0 ):
-        self.__nlos_loss = 20
-        self.__atmospheric_loss = 1
-        self.__polarization_loss = 3
-        self.__line_of_sight_prob = line_of_sight_prob
+    
+    def __init__(self):
+        super().__init__()
+        self.nlos_loss = 20
+        self.atmospheric_loss = 1
+        self.polarization_loss = 3
 
+        
     def get_loss(self, *args, **kwargs) -> np.array:
         d = np.asarray(kwargs["distance"])
         f = np.asarray(kwargs["frequency"])
+        p_los = kwargs["line_of_sight_prob"]
+        
         free_space_loss = 20*np.log10(d) + 20*np.log10(f) - 27.55
-        line_of_sight = np.random.sample(d.shape) <= self.__line_of_sight_prob
-        nlos_loss = self.__nlos_loss * ( ~line_of_sight )
-        loss = (free_space_loss + nlos_loss + self.__polarization_loss
-                + self.__atmospheric_loss)
+        line_of_sight = np.random.sample(d.shape) <= p_los
+        nlos_loss = self.nlos_loss * ( ~line_of_sight )
+        loss = (free_space_loss + nlos_loss + self.polarization_loss
+                + self.atmospheric_loss)
+        
         return loss
