4b57722c269dee0e136a7a4ad98612547a733b51
edgar <edgar@anatel.gov.br>
2017-07-06 14:35:20 -0300

    Removing code of omni antennas. UE distribution and pointing in simulations scenario according to agreements in TG 5/1 meeting.

--- sharc/station_factory.py
+++ sharc/station_factory.py
@@ -21,50 +21,35 @@ class StationFactory(object):
     def generate_imt_base_stations(param: ParametersImt, 
                                    param_ant: ParametersAntennaImt,
                                    topology: Topology):
-        num_bs = param.num_clusters*param.num_base_stations
-        if(param_ant.bs_tx_antenna_type == "BEAMFORMING" or \
-           param_ant.bs_rx_antenna_type == "BEAMFORMING"):
-            num_bs = 3*num_bs
+        num_bs = topology.num_base_stations
         imt_base_stations = StationManager(num_bs)
         
         # now we set the coordinates
-        if(param_ant.bs_rx_antenna_type == "BEAMFORMING"):
-            imt_base_stations.x = np.repeat(topology.x,3)
-            imt_base_stations.y = np.repeat(topology.y,3)
-        else:
-            imt_base_stations.x = topology.x
-            imt_base_stations.y = topology.y    
+        imt_base_stations.x = topology.x
+        imt_base_stations.y = topology.y
+        imt_base_stations.azimuth = topology.azimuth
+        imt_base_stations.elevation = topology.elevation
         imt_base_stations.height = param.bs_height*np.ones(num_bs)
         
         imt_base_stations.active = np.ones(num_bs)
         imt_base_stations.tx_power = param.bs_tx_power*np.ones(num_bs)
         imt_base_stations.rx_interference = -500*np.ones(param.ue_k)
         
-        if(param_ant.bs_tx_antenna_type == "OMNI"):
-            imt_base_stations.tx_antenna = \
-                np.array([AntennaOmni(param_ant.bs_tx_omni_antenna_gain) \
-                          for i in range(num_bs)])
-        elif(param_ant.bs_tx_antenna_type == "BEAMFORMING"):
-            imt_base_stations.tx_antenna = np.empty(num_bs,dtype=AntennaBeamformingImt)
-            par = param_ant.get_antenna_parameters("BS","TX")
-            
-            for i in range(num_bs):
-                imt_base_stations.tx_antenna[i] = \
-                AntennaBeamformingImt(par,param_ant.bs_tx_azimuth[i%3],\
-                                      param_ant.bs_tx_elevation)
+        imt_base_stations.tx_antenna = np.empty(num_bs, dtype=AntennaBeamformingImt)
+        par = param_ant.get_antenna_parameters("BS", "TX")
         
-        if(param_ant.bs_rx_antenna_type == "OMNI"):
-            imt_base_stations.rx_antenna = \
-                np.array([AntennaOmni(param_ant.bs_rx_omni_antenna_gain) \
-                          for i in range(num_bs)])
-        elif(param_ant.bs_rx_antenna_type == "BEAMFORMING"):
-            imt_base_stations.rx_antenna = np.empty(num_bs,dtype=AntennaBeamformingImt)
-            par = param_ant.get_antenna_parameters("BS","RX")
-            
-            for i in range(num_bs):
-                imt_base_stations.rx_antenna[i] = \
-                AntennaBeamformingImt(par,param_ant.bs_rx_azimuth[i%3],\
-                                      param_ant.bs_rx_elevation)
+        for i in range(num_bs):
+            imt_base_stations.tx_antenna[i] = \
+            AntennaBeamformingImt(par, imt_base_stations.azimuth[i],\
+                                  imt_base_stations.elevation[i])
+        
+        imt_base_stations.rx_antenna = np.empty(num_bs, dtype=AntennaBeamformingImt)
+        par = param_ant.get_antenna_parameters("BS", "RX")
+        
+        for i in range(num_bs):
+            imt_base_stations.rx_antenna[i] = \
+            AntennaBeamformingImt(par, imt_base_stations.azimuth[i],\
+                                  imt_base_stations.elevation[i])
             
         imt_base_stations.bandwidth = param.bandwidth*np.ones(num_bs)
         imt_base_stations.noise_figure = param.bs_noise_figure*np.ones(num_bs)
@@ -72,120 +57,90 @@ class StationFactory(object):
         imt_base_stations.is_satellite = False
         return imt_base_stations
 
+        
     @staticmethod
     def generate_imt_ue(param: ParametersImt, 
                         param_ant: ParametersAntennaImt,
                         topology: Topology):
-        num_ue = param.num_clusters*param.num_base_stations*param.ue_k*param.ue_k_m
-        if(param_ant.bs_tx_antenna_type == "BEAMFORMING" or \
-           param_ant.bs_rx_antenna_type == "BEAMFORMING"):
-            num_ue = 3*num_ue
+        num_bs = topology.num_base_stations
+        num_ue_per_bs = param.ue_k*param.ue_k_m
+        num_ue = num_bs*num_ue_per_bs
+
         imt_ue = StationManager(num_ue)
-        #imt_ue.x = (topology.x_max - topology.x_min)*np.random.random(num_ue) + topology.x_min
-        #imt_ue.y = (topology.y_max - topology.y_min)*np.random.random(num_ue) + topology.y_min
         ue_x = list()
         ue_y = list()
+        
+        # The Rayleigh and Normal distribution parameters (mean, scale and cutoff)
+        # were agreed in TG 5/1 meeting (May 2017).
+        
+        # For the distance between UE and BS, it is desired that 99% of UE's 
+        # are located inside the [soft] cell edge, i.e. Prob(d<d_edge) = 99%.
+        # Since the distance is modeled by a random variable with Rayleigh
+        # distribution, we use the quantile function to find that 
+        # sigma = distance/3.0345. So we always distibute UE's in order to meet
+        # the requirement Prob(d<d_edge) = 99% for a given cell radius.
+        radius_scale = topology.cell_radius/3.0345
+        radius = np.random.rayleigh(radius_scale, num_ue)
+        
+        # In case of the angles, we generate N times the number of UE's because 
+        # the angle cutoff will discard 5% of the terminals whose angle is 
+        # outside the angular sector defined by [-60, 60]. So, N = 1.4 seems to
+        # be a safe choice.
+        N = 1.4
+        angle_scale = 30
+        angle_mean = 0
+        angle_n = np.random.normal(angle_mean, angle_scale, int(N*num_ue))
+        
+        angle_cutoff = 60
+        idx = np.where((angle_n < angle_cutoff) & (angle_n > -angle_cutoff))[0][:num_ue]
+        angle = angle_n[idx]
 
-        if(param_ant.bs_rx_antenna_type == "BEAMFORMING" or param_ant.bs_rx_antenna_type == "BEAMFORMING"):
-            beamforming = True
-            cell_r = topology.intersite_distance/3
-            num_bs = 3*topology.x.size
-            bs_x = np.repeat(topology.x,3)
-            bs_y = np.repeat(topology.y,3)
-        else:
-            beamforming = False
-            cell_r = topology.cell_radius
-            num_bs = topology.x.size
-            bs_x = topology.x
-            bs_y = topology.y
-            
+        # Calculate UE pointing
+        azimuth_range = (-60, 60)
+        azimuth = (azimuth_range[1] - azimuth_range[0])*np.random.random(num_ue) + azimuth_range[0]
+        # Remove the randomness from azimuth and you will have a perfect pointing
+        #azimuth = np.zeros(num_ue)
+        elevation_range = (-90, 90)
+        elevation = (elevation_range[1] - elevation_range[0])*np.random.random(num_ue) + elevation_range[0]                   
+
         for bs in range(num_bs):
-            if(beamforming):
-                i = bs%3
-                ang = np.deg2rad(60+i*120)
-                x_center = cell_r*np.cos(ang) + bs_x[bs]
-                y_center = cell_r*np.sin(ang) + bs_y[bs]
-
-                # UE's are generated inside a inscribed circle of a regular hexagon (sector)
-                r_max = cell_r*np.sqrt(3)/2
-                x_min = x_center - r_max
-                x_max = x_center + r_max
-                y_min = y_center - r_max
-                y_max = y_center + r_max                
-                scale = 10
-                x = (x_max - x_min)*np.random.random(scale*param.ue_k*param.ue_k_m) + x_min
-                y = (y_max - y_min)*np.random.random(scale*param.ue_k*param.ue_k_m) + y_min
-            
-                r = np.sqrt((x - x_center)**2 + (y - y_center)**2)
-
-                # 2D separation distance between BS and UE
-                r_sep_min = param.minimum_separation_distance_bs_ue
-                r_sep = np.sqrt((x - bs_x[bs])**2 + (y - bs_y[bs])**2)
-                
-                idx = np.where((r < r_max) & (r_sep > r_sep_min))[0]
-                i = idx[:param.ue_k*param.ue_k_m]
-                x = x[i]
-                y = y[i]
-    
-            else:
-                x_min = bs_x[bs] - cell_r
-                x_max = bs_x[bs] + cell_r
-                y_min = bs_y[bs] - cell_r
-                y_max = bs_y[bs] + cell_r
-                x = (x_max - x_min)*np.random.random(param.ue_k*param.ue_k_m) + x_min
-                y = (y_max - y_min)*np.random.random(param.ue_k*param.ue_k_m) + y_min
+            idx = [i for i in range(bs*num_ue_per_bs, bs*num_ue_per_bs + num_ue_per_bs)]
+            # theta is the horizontal angle of the UE wrt the serving BS
+            theta = topology.azimuth[bs] + angle[idx]
+            # calculate UE position in x-y coordinates
+            x = topology.x[bs] + radius[idx]*np.cos(np.radians(theta))
+            y = topology.y[bs] + radius[idx]*np.sin(np.radians(theta))
             ue_x.extend(x)
             ue_y.extend(y)
+            # calculate UE azimuth wrt serving BS
+            imt_ue.azimuth[idx] = (azimuth[idx] + theta + 180)%360
+
+            # calculate elevation angle
+            # psi is the vertical angle of the UE wrt the serving BS
+            distance = np.sqrt((topology.x[bs] - x)**2 + (topology.y[bs] - y)**2)
+            psi = np.degrees(np.arctan((param.bs_height - param.ue_height)/distance))
+            imt_ue.elevation[idx] = elevation[idx] + psi
+
         imt_ue.x = np.array(ue_x)
         imt_ue.y = np.array(ue_y)
+
         imt_ue.active = np.zeros(num_ue, dtype=bool)
         imt_ue.height = param.ue_height*np.ones(num_ue)
         imt_ue.tx_power = param.ue_tx_power*np.ones(num_ue)
         imt_ue.rx_interference = -500*np.ones(num_ue)
 
-        if(param_ant.ue_tx_antenna_type == "OMNI"):
-            imt_ue.tx_antenna = \
-                np.array([AntennaOmni(param_ant.ue_tx_omni_antenna_gain) \
-                          for i in range(num_ue)])
-        elif(param_ant.ue_tx_antenna_type == "BEAMFORMING"):
-            imt_ue.tx_antenna = np.empty(num_ue,dtype=AntennaBeamformingImt)
-            par = param_ant.get_antenna_parameters("UE","TX")
-            
-            if(param_ant.ue_tx_pointing == "FIXED"):
-                azi = param_ant.ue_tx_azimuth
-                ele = param_ant.ue_tx_elevation
-            elif(param_ant.ue_tx_pointing == "RANDOM"):
-                azi = np.random.uniform(-180,180)
-                ele = np.random.uniform(0,180)
-                
-            for i in range(num_ue):
-                imt_ue.tx_antenna[i] = \
-                AntennaBeamformingImt(par,azi,ele)
-                
-        if(param_ant.ue_rx_antenna_type == "OMNI"):
-            imt_ue.rx_antenna = \
-                np.array([AntennaOmni(param_ant.ue_rx_omni_antenna_gain) \
-                          for i in range(num_ue)])
-        elif(param_ant.ue_rx_antenna_type == "BEAMFORMING"):
-            imt_ue.rx_antenna = np.empty(num_ue,dtype=AntennaBeamformingImt)
-            par = param_ant.get_antenna_parameters("UE","RX")
-            
-            if(param_ant.ue_rx_pointing == "FIXED"):
-                azi = param_ant.ue_rx_azimuth
-                ele = param_ant.ue_rx_elevation
-            elif(param_ant.ue_rx_pointing == "RANDOM"):
-                azi = np.random.uniform(-180,180)
-                ele = np.random.uniform(0,180)
-                
-            for i in range(num_ue):
-                imt_ue.rx_antenna[i] = \
-                AntennaBeamformingImt(par,azi,ele)
+        # TODO: this piece of code works only for uplink
+        par = param_ant.get_antenna_parameters("UE","TX")
+        for i in range(num_ue):
+            imt_ue.tx_antenna[i] = AntennaBeamformingImt(par, imt_ue.azimuth[i], 
+                                                         imt_ue.elevation[i])
             
         imt_ue.bandwidth = param.bandwidth*np.ones(num_ue)
         imt_ue.noise_figure = param.ue_noise_figure*np.ones(num_ue)
         imt_ue.is_satellite = False
         return imt_ue
 
+        
     @staticmethod
     def generate_fss_stations(param: ParametersFss):
         satellite_stations = StationManager(1)
