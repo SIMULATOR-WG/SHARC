32ccc439fef1e7389ada9f63a7a0d7f5f9a595bd
edgar <edgar@anatel.gov.br>
2017-07-07 18:00:20 -0300

    Refactoring class

--- sharc/topology/topology_single_base_station.py
+++ sharc/topology/topology_single_base_station.py
@@ -10,36 +10,50 @@ from sharc.topology.topology import Topology
 
 class TopologySingleBaseStation(Topology):
     """
-    Generates the a single base station centered at (0,0).
+    Generates the a single base station centered at (0,0), with azimuth = 0°
+    and elevation = -10° wrt x-y plane.
     """
     
+    # possible values for base station azimuth and elevation [degrees]
+    AZIMUTH = 0
+    ELEVATION = -10
+    ALLOWED_NUM_CLUSTERS = [1, 2]
+    
+
     def __init__(self, cell_radius: float, num_clusters: int):
         """
-        Constructor method that sets the parameters and already calls the 
-        calculation methods.
+        Constructor method that sets the object attributes
         
         Parameters
         ----------
             cell_radius : radius of the cell
         """
-        allowed_num_clusters = [1,2]
-        # Actually, intersite distance is not used
+        if num_clusters not in TopologySingleBaseStation.ALLOWED_NUM_CLUSTERS:
+            error_message = "invalid number of clusters ({})".format(num_clusters)
+            raise ValueError(error_message)   
+            
         intersite_distance = 2*cell_radius
-        super().__init__(intersite_distance, cell_radius, num_clusters, allowed_num_clusters)
+        super().__init__(intersite_distance, cell_radius)
+        self.num_clusters = num_clusters
+        
         
     def calculate_coordinates(self):
         """
-        Defines the coordinates of the station.
+        Defines the coordinates of the base stations.
         """        
-        if self.num_clusters == 1:
-            self.x = np.array([0])
-            self.y = np.array([0])
-        elif self.num_clusters == 2:
-            self.x = np.array([-self.cell_radius, self.cell_radius])
-            self.y = np.array([0, 0])
-        else:
-            error_message = "invalid number of clusters ({})".format(self.num_clusters)
-            raise ValueError(error_message)             
-        
+        if not self.static_base_stations:
+            self.static_base_stations = True
+            if self.num_clusters == 1:
+                self.x = np.array([0])
+                self.y = np.array([0])
+                self.azimuth = TopologySingleBaseStation.AZIMUTH
+                self.elevation = TopologySingleBaseStation.ELEVATION
+                self.num_base_stations = 1
+            elif self.num_clusters == 2:
+                self.x = np.array([0, 0])
+                self.y = np.array([0, self.intersite_distance])
+                self.azimuth = TopologySingleBaseStation.AZIMUTH*np.ones(2)
+                self.elevation = TopologySingleBaseStation.ELEVATION*np.ones(2)
+                self.num_base_stations = 2             
 
  
