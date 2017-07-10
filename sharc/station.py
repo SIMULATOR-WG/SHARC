596ea313d917f44a80d5b4cafdaf79a28269a6f9
edgar <edgar@anatel.gov.br>
2017-07-10 14:58:00 -0300

    Refactoring Station class and removing unused properties.

--- sharc/station.py
+++ sharc/station.py
@@ -6,99 +6,32 @@ Created on Mon Feb  6 10:51:19 2017
 """
 
 from sharc.support.enumerations import StationType
-from sharc.antenna.antenna import Antenna
 
 class Station(object):
     
     def __init__(self):
-        self.__id = -1
-        self.__station_type = StationType.NONE
-        self.__x = 0
-        self.__y = 0
-        self.__height = 0
-        self.__tx_power = 0
-        self.__rx_power = 0
-        self.__antenna = Antenna()
-        # test attributes below
-        self.__active = False
-        self.__acs = 0
-        self.__aclr = 0
-        self.__sensitivity = 0
-        self.__noise_figure = 0
-        self.__rx_aggr_interference = -300
-
-    @property
-    def id(self):
-        return self.__id
+        self.id = -1
+        self.x = 0
+        self.y = 0
+        self.azimuth = 0
+        self.elevation = 0
+        self.height = 0
+        self.active = False
+        self.tx_power = 0
+        self.rx_power = 0
+        self.rx_interference = 0
+        self.antenna = None
+        self.bandwidth = 0
+        self.noise_figure = 0
+        self.noise_temperature = 0
+        self.thermal_noise = 0
+        self.total_interference = 0
+        self.snr = 0
+        self.sinr = 0
+        self.inr = 0
+        self.station_type = StationType.NONE
         
-    @id.setter
-    def id(self, value):
-        self.__id = value
         
-    @property
-    def station_type(self):
-        return self.__station_type
-    
-    @station_type.setter
-    def station_type(self, value):
-        self.__station_type = value
-        
-    @property
-    def x(self):
-        return self.__x
-        
-    @x.setter
-    def x(self, value):
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-        
-    @y.setter
-    def y(self, value):
-        self.__y = value
-        
-    @property
-    def height(self):
-        return self.__height
-        
-    @height.setter
-    def height(self, value):
-        self.__height = value
-        
-    @property
-    def active(self):
-        return self.__active
-        
-    @active.setter
-    def active(self, value):
-        self.__active = value
-        
-    @property
-    def tx_power(self):
-        return self.__tx_power
-        
-    @tx_power.setter
-    def tx_power(self, value):
-        self.__tx_power = value
-
-    @property
-    def rx_power(self):
-        return self.__rx_power
-        
-    @rx_power.setter
-    def rx_power(self, value):
-        self.__rx_power = value
-
-    @property
-    def antenna(self):
-        return self.__antenna
-        
-    @antenna.setter
-    def antenna(self, value):
-        self.__antenna = value
-    
     def __eq__(self, other):
         if isinstance(other, self.__class__):
             equal = (self.id == other.id and 
@@ -109,6 +42,7 @@ class Station(object):
         else:
             return NotImplemented
 
+            
     def __ne__(self, other):
         if isinstance(other, self.__class__):
             not_equal = (self.id != other.id or 
@@ -118,6 +52,7 @@ class Station(object):
             return not_equal
         else:
             return NotImplemented
+            
             
     def __hash__(self):
         return hash(self.id, self.x, self.y, self.height)
