84a1faa30bb4c8860212d371e0761857e0efa65c
edgar <edgar@anatel.gov.br>
2017-07-10 14:55:41 -0300

    Refactoring Propagation class and making it a subclass of ABC.

--- sharc/propagation/propagation.py
+++ sharc/propagation/propagation.py
@@ -5,15 +5,13 @@ Created on Thu Feb 16 12:03:12 2017
 @author: edgar
 """
 
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import numpy as np
  
-class Propagation(object):
+class Propagation(ABC):
     """
     Abstract base class for propagation models
     """
-    
-    __metaclass__ = ABCMeta
     
     @abstractmethod
     def get_loss(self, *args, **kwargs) -> np.array:
