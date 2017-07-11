f976666ddfb26cabb5cc261e0cbe2a9e0dba8d83
CalilQ <calil_queiroz@hotmail.com>
2017-07-11 15:12:56 -0300

    Initial implementation of beamforming tests

--- tests/test_simulation_uplink.py
+++ tests/test_simulation_uplink.py
@@ -14,6 +14,7 @@ from sharc.parameters.parameters_imt import ParametersImt
 from sharc.parameters.parameters_fss import ParametersFss
 from sharc.parameters.parameters_antenna_imt import ParametersAntennaImt
 from sharc.antenna.antenna_omni import AntennaOmni
+from sharc.antenna.antenna_beamforming_imt import AntennaBeamformingImt
 from sharc.station_factory import StationFactory
 
 class SimulationUplinkTest(unittest.TestCase):
@@ -63,42 +64,42 @@ class SimulationUplinkTest(unittest.TestCase):
         self.param.BOLTZMANN_CONSTANT = 1.38064852e-23
         
         self.param_ant = ParametersAntennaImt()
-        self.param_ant.bs_tx_element_max_g = 5
+        self.param_ant.bs_tx_element_max_g = 10
         self.param_ant.bs_tx_element_phi_3db = 80
-        self.param_ant.bs_tx_element_theta_3db = 65
-        self.param_ant.bs_tx_element_am = 30
-        self.param_ant.bs_tx_element_sla_v = 30
-        self.param_ant.bs_tx_n_rows = 8
-        self.param_ant.bs_tx_n_columns = 8
-        self.param_ant.bs_tx_element_horiz_spacing = 0.5
-        self.param_ant.bs_tx_element_vert_spacing = 0.5
+        self.param_ant.bs_tx_element_theta_3db = 80
+        self.param_ant.bs_tx_element_am = 25
+        self.param_ant.bs_tx_element_sla_v = 25
+        self.param_ant.bs_tx_n_rows = 16
+        self.param_ant.bs_tx_n_columns = 16
+        self.param_ant.bs_tx_element_horiz_spacing = 1
+        self.param_ant.bs_tx_element_vert_spacing = 1
         self.param_ant.bs_rx_element_max_g = 5
         self.param_ant.bs_rx_element_phi_3db = 65
         self.param_ant.bs_rx_element_theta_3db = 65
         self.param_ant.bs_rx_element_am = 30
         self.param_ant.bs_rx_element_sla_v = 30
-        self.param_ant.bs_rx_n_rows = 8
-        self.param_ant.bs_rx_n_columns = 8
+        self.param_ant.bs_rx_n_rows = 2
+        self.param_ant.bs_rx_n_columns = 2
         self.param_ant.bs_rx_element_horiz_spacing = 0.5
         self.param_ant.bs_rx_element_vert_spacing = 0.5
         self.param_ant.ue_tx_element_max_g = 5
-        self.param_ant.ue_tx_element_phi_3db = 80
+        self.param_ant.ue_tx_element_phi_3db = 65
         self.param_ant.ue_tx_element_theta_3db = 65
         self.param_ant.ue_tx_element_am = 30
         self.param_ant.ue_tx_element_sla_v = 30
-        self.param_ant.ue_tx_n_rows = 4
-        self.param_ant.ue_tx_n_columns = 4
+        self.param_ant.ue_tx_n_rows = 2
+        self.param_ant.ue_tx_n_columns = 1
         self.param_ant.ue_tx_element_horiz_spacing = 0.5
         self.param_ant.ue_tx_element_vert_spacing = 0.5
-        self.param_ant.ue_rx_element_max_g = 5
+        self.param_ant.ue_rx_element_max_g = 10
         self.param_ant.ue_rx_element_phi_3db = 90
         self.param_ant.ue_rx_element_theta_3db = 90
         self.param_ant.ue_rx_element_am = 25
         self.param_ant.ue_rx_element_sla_v = 25
-        self.param_ant.ue_rx_n_rows = 4
-        self.param_ant.ue_rx_n_columns = 4
-        self.param_ant.ue_rx_element_horiz_spacing = 0.5
-        self.param_ant.ue_rx_element_vert_spacing = 0.5
+        self.param_ant.ue_rx_n_rows = 16
+        self.param_ant.ue_rx_n_columns = 16
+        self.param_ant.ue_rx_element_horiz_spacing = 1
+        self.param_ant.ue_rx_element_vert_spacing = 1
         
         self.simulation = SimulationUplink(self.param, ParametersFss(), self.param_ant)
         self.simulation.initialize()
@@ -134,9 +135,52 @@ class SimulationUplinkTest(unittest.TestCase):
         
 
 
-    def test_calculate_gains(self):
-        pass
-    
+    def test_beamforming_gains(self):
+        eps = 1e-2
+        
+        # Set scenario
+        self.simulation.bs = StationFactory.generate_imt_base_stations(self.param,
+                                                                       self.param_ant,
+                                                                       self.simulation.topology)
+        
+        self.simulation.ue = StationFactory.generate_imt_ue(self.param,
+                                                            self.param_ant,
+                                                            self.simulation.topology)
+        self.simulation.ue.x = np.array([50.000, 150.000, 43.301, 25.000])
+        self.simulation.ue.y = np.array([ 0.000,   0.000, 25.000, 43.301])
+        
+        # Physical pointing angles
+        self.assertEqual(self.simulation.bs.antenna[0].azimuth,0)
+        self.assertEqual(self.simulation.bs.antenna[0].elevation,-10)
+        self.assertEqual(self.simulation.bs.antenna[1].azimuth,180)
+        self.assertEqual(self.simulation.bs.antenna[0].elevation,-10)
+        
+        # Change UE pointing
+        self.simulation.ue.azimuth = np.array([180, 90, 30, -30])
+        self.simulation.ue.elevation = np.array([-30, -15, 15, 30])
+        par = self.param_ant.get_antenna_parameters("UE","TX")
+        for i in range(self.simulation.ue.num_stations):
+            self.simulation.ue.antenna[i] = AntennaBeamformingImt(par, self.simulation.ue.azimuth[i], 
+                                                                  self.simulation.ue.elevation[i])
+        self.assertEqual(self.simulation.ue.antenna[0].azimuth,180)
+        self.assertEqual(self.simulation.ue.antenna[0].elevation,-30)
+        self.assertEqual(self.simulation.ue.antenna[1].azimuth,90)
+        self.assertEqual(self.simulation.ue.antenna[1].elevation,-15)
+        self.assertEqual(self.simulation.ue.antenna[2].azimuth,30)
+        self.assertEqual(self.simulation.ue.antenna[2].elevation,15)
+        self.assertEqual(self.simulation.ue.antenna[3].azimuth,-30)
+        self.assertEqual(self.simulation.ue.antenna[3].elevation,30)
+        
+        # Simulate connection and selection
+        self.simulation.connect_ue_to_bs()
+        self.simulation.select_ue()
+        # Deactivate some stations for test
+        self.simulation.ue.active = [True, False, False, True]
+        
+        # Test gains
+        ref_gain = np.array([[]])
+        gain = self.simulation.calculate_gains(self.simulation.bs,self.simulation.ue)
+#        npt.assert_allclose(gain,ref_gain,atol=eps)
     
     def test_calculate_coupling_loss(self):
         pass
@@ -144,8 +188,6 @@ class SimulationUplinkTest(unittest.TestCase):
     
     def test_calculate_imt_ul_tput(self):
         pass
-    
-    
     
                 
 if __name__ == '__main__':
