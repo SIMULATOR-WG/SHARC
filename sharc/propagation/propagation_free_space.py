fd658bd5d67ba945562fef3d00fb143932aba199
edgar <edgar@anatel.gov.br>
2017-07-12 15:24:27 -0300

    Using 3D distance if 2D distance is not found.

--- sharc/propagation/propagation_free_space.py
+++ sharc/propagation/propagation_free_space.py
@@ -18,7 +18,10 @@ class PropagationFreeSpace(Propagation):
         super().__init__()
     
     def get_loss(self, *args, **kwargs) -> np.array:
-        d = np.asarray(kwargs["distance_2D"])
+        if "distance_2D" in kwargs:
+            d = np.asarray(kwargs["distance_2D"])
+        else:
+            d = np.asarray(kwargs["distance_3D"])
         f = np.asarray(kwargs["frequency"])
         loss = 20*np.log10(d) + 20*np.log10(f) - 27.55
         return loss
