03dbbcd7b5657e5a8dc43cad14525d07146d4f2c
edgar <edgar@anatel.gov.br>
2017-07-12 19:40:34 -0300

    Adding support for UMi and ABG path loss models.

--- sharc/propagation/propagation_factory.py
+++ sharc/propagation/propagation_factory.py
@@ -13,7 +13,8 @@ from sharc.propagation.propagation_close_in import PropagationCloseIn
 from sharc.propagation.propagation_p619 import PropagationP619
 from sharc.propagation.propagation_sat_simple import PropagationSatSimple
 from sharc.propagation.propagation_uma import PropagationUMa
-#from sharc.propagation.propagation_umi import PropagationUMi
+from sharc.propagation.propagation_umi import PropagationUMi
+from sharc.propagation.propagation_abg import PropagationABG
 
 class PropagationFactory(object):
     
@@ -21,10 +22,12 @@ class PropagationFactory(object):
     def createPropagation(channel_model: str) -> Propagation:
         if channel_model == "FSPL":
             return PropagationFreeSpace()
+        elif channel_model == "ABG":
+            return PropagationABG()
         elif channel_model == "UMa":
             return PropagationUMa()
-#        elif channel_model == "UMi":
-#            return PropagationUMi()            
+        elif channel_model == "UMi":
+            return PropagationUMi()            
         elif channel_model == "CI":
             return PropagationCloseIn()
         elif channel_model == "SatelliteSimple":
