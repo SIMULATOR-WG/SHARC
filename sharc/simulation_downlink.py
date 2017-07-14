cde4fce4743a08df28184888fa9b327d0a60809f
edgar <edgar@anatel.gov.br>
2017-07-14 19:46:15 -0300

    Initial version of IMT downlink.

--- sharc/simulation_downlink.py
+++ sharc/simulation_downlink.py
@@ -6,141 +6,81 @@ Created on Wed Jan 11 19:06:41 2017
 """
 
 import numpy as np
-import random
 import math
-import sys
 
 from sharc.simulation import Simulation
 from sharc.parameters.parameters_imt import ParametersImt
 from sharc.parameters.parameters_antenna_imt import ParametersAntennaImt
+from sharc.parameters.parameters_fss import ParametersFss
 from sharc.station_factory import StationFactory
-from sharc.topology.topology_factory import TopologyFactory
-from sharc.propagation.propagation_free_space import PropagationFreeSpace
-from sharc.propagation.propagation_close_in import PropagationCloseIn
-from sharc.results import Results
 
 class SimulationDownlink(Simulation):
     """
     Implements the flowchart of simulation downlink method
     """
 
-    def __init__(self, param: ParametersImt, param_ant: ParametersAntennaImt):
-        super(SimulationDownlink, self).__init__()
-        np.random.seed(0)
+    def __init__(self, param_imt: ParametersImt, param_system: ParametersFss, param_ant: ParametersAntennaImt):
+        super().__init__(param_imt, param_system, param_ant)
 
-        self.param = param
-        self.param_imt_antenna = param_ant
-
-        self.topology = TopologyFactory.createTopology(self.param)
-
-        if self.param.channel_model == "FSPL":
-            self.propagation = PropagationFreeSpace()
-        elif self.param.channel_model == "CI":
-            self.propagation = PropagationCloseIn( self.param.topology,
-                                                   self.param.line_of_sight_prob )
-        else:
-            sys.stderr.write("error: invalid parameter channel_model\n")
-            sys.exit(1)
-
-
-        self.num_transmitters = self.param.num_clusters*self.param.num_base_stations
-        self.num_receivers = self.param.num_clusters*self.param.num_base_stations \
-                                 *self.param.ue_k*self.param.ue_k_m
-
-        self.coupling_loss = np.empty([self.num_transmitters, self.num_receivers])
-        self.transmitter = np.empty(self.num_transmitters)
-        self.receiver = np.empty(self.num_receivers)
-        self.link = dict([(bs,list()) for bs in range(self.num_transmitters)])
-        self.results = Results()
-
-    def initialize(self, *args, **kwargs):
-        pass
-        self.transmitter = \
-            StationFactory.generate_imt_base_stations(self.param,
-                                                      self.param_imt_antenna,
-                                                      self.topology)
-
+        
     def snapshot(self, *args, **kwargs):
-        if not self.param.static_base_stations:
-            # TODO: include support for randomly located base stations by
-            # creating the RandomTopology(Topology) class
-            pass
-        if self.param.interfered_with:
-            self.create_ue()
-            self.calculate_coupling_loss()
-            self.connect_ue_to_bs()
-            self.select_ue()
-            self.scheduler()
-            self.power_control()
-            self.calculate_sinr()
+        write_to_file = kwargs["write_to_file"]
+        snapshot_number = kwargs["snapshot_number"]
+        
+        # In case of hotspots, base stations coordinates have to be calculated
+        # on every snapshot. Anyway, let topology decide whether to calculate
+        # or not
+        self.topology.calculate_coordinates()
+        
+        # Create the base stations (remember that it takes into account the
+        # network load factor)
+        self.bs = StationFactory.generate_imt_base_stations(self.param_imt,
+                                                            self.param_imt_antenna,
+                                                            self.topology)      
+        
+        # Create the other system (FSS, HAPS, etc...)
+        # Currently it supports only FSS space station
+        self.system = StationFactory.generate_fss_space_stations(self.param_system)
+
+        # Create IMT user equipments
+        self.ue = StationFactory.generate_imt_ue(self.param_imt,
+                                                 self.param_imt_antenna,
+                                                 self.topology)
+        #self.plot_scenario()
+        
+        self.connect_ue_to_bs()
+        self.select_ue()
+        
+        # Calculate coupling loss after beams are created
+        self.coupling_loss_imt = self.calculate_coupling_loss(self.bs, 
+                                                              self.ue,
+                                                              self.propagation_imt)
+        self.scheduler()
+        self.power_control()
+        
+        if self.param_imt.interfered_with:
+            # Execute this piece of code if the other system generates 
+            # interference into IMT
+            #self.calculate_sinr()
             #self.add_external_interference()
             #self.recalculate_sinr()
             #self.calculate_imt_degradation()
-        else:
-            #self.beamforming()
-            #self.apply_power_control()
-            #self.scheduler()
-            #self.select_active_bs()
-            #self.calculate_other_interference()
-            #self.calculate_other_degradation()
             pass
-        self.collect_results()
+        else:
+            # Execute this piece of code if IMT generates interference into
+            # the other system
+            self.calculate_sinr()
+            #self.calculate_external_interference()
+            #self.calculate_external_degradation()
+            pass
+        
+        self.collect_results(write_to_file, snapshot_number)
 
+
+        
     def finalize(self, *args, **kwargs):
         self.notify_observers(source=__name__, results=self.results)
 
-    def create_ue(self):
-        self.receiver = StationFactory.generate_imt_ue(self.param,
-                                                       self.param_imt_antenna,
-                                                       self.topology)
-
-    def calculate_coupling_loss(self):
-        # Calculate distance from transmitters to receivers. The result is a
-        # num_tx x num_rx array
-        d = self.transmitter.get_distance_to(self.receiver)
-        path_loss = self.propagation.get_loss(distance=d, frequency=self.param.frequency)
-        tx_antenna = self.transmitter.tx_antenna.astype('float')
-        rx_antenna = self.receiver.rx_antenna.astype('float')
-        # replicate columns to have the appropriate size
-        tx_gain = np.transpose(np.tile(tx_antenna, (self.receiver.num_stations, 1)))
-        rx_gain = np.tile(rx_antenna, (self.transmitter.num_stations, 1))
-        # calculate coupling loss
-        self.coupling_loss = path_loss - tx_gain - rx_gain
-
-    def connect_ue_to_bs(self):
-        """
-        Link the UE randomly to a BS to which the path coupling loss is within
-        the minimum coupling loss plus the HO margin.
-        """
-        for ue in range(self.num_receivers):
-            # for each receiver, select the base stations
-            minimum_coupling_loss = np.amin(self.coupling_loss[:,ue])
-            bs_all = np.where(self.coupling_loss[:,ue] < minimum_coupling_loss + self.param.ho_margin)[0]
-            bs = np.random.choice(bs_all)
-            self.link[bs].append(ue)
-
-    def select_ue(self):
-        """
-        Select K UEs randomly from all the UEs linked to one BS as “chosen”
-        UEs. These K “chosen” UEs will be scheduled during this snapshot.
-        """
-        for bs in range(self.num_transmitters):
-            # select K UE's among the ones that are connected to BS
-            random.shuffle(self.link[bs])
-            K = self.param.ue_k
-            #K = np.random.choice(range(1, len(self.link[bs])))
-            del self.link[bs][K:]
-            # Activate the selected UE's
-            self.receiver.active[self.link[bs]] = np.ones(K, dtype=bool)
-
-    def scheduler(self):
-        """
-        This scheduler divides the available bandwidth among UE's ona given BS
-        """
-        for bs in range(self.num_transmitters):
-            ue = self.link[bs]
-            bandwidth = self.param.bandwidth / len(ue)
-            self.receiver.bandwidth[ue] = bandwidth
 
     def power_control(self):
         """
@@ -148,53 +88,66 @@ class SimulationDownlink(Simulation):
         """
         # Currently, the maximum transmit power of the base station is equaly
         # divided among the selected UEs
-        p_max = math.pow(10, 0.1*self.param.bs_tx_power)
+        p_max = math.pow(10, 0.1*self.param_imt.bs_tx_power)
+        tx_power = 10*math.log10(p_max/self.param_imt.ue_k)
         # calculate tansmit powers to have a structure such as
         # {bs_1: [pwr_1, pwr_2,...], ...}, where bs_1 is the base station id,
         # pwr_1 is the transmit power from bs_1 to ue_1, pwr_2 is the transmit
         # power from bs_1 to ue_2, etc
-        self.transmitter.tx_power = dict([(bs,[10*math.log10(p_max/len(self.link[bs]))] * len(self.link[bs])) for bs in range(self.num_transmitters)])
+        bs_active = np.where(self.bs.active)[0]
+        self.bs.tx_power = dict([(bs, tx_power*np.ones(self.param_imt.ue_k)) for bs in bs_active])
 
+        
     def calculate_sinr(self):
-        bs_all = [b for b in range(self.num_transmitters)]
+        """
+        Calculates the downlink SINR for each UE. 
+        """        
+        bs_active = np.where(self.bs.active)[0]
+        for bs in bs_active:
+            ue = self.link[bs]
+            self.ue.rx_power[ue] = self.bs.tx_power[bs] - self.coupling_loss_imt[bs,ue] \
+                                     - self.param_imt.ue_body_loss - self.param_imt.ue_feed_loss - self.param_imt.bs_feed_loss
 
-        # calculate received power for each UE
-        for bs, ue_list in self.link.items():
-            self.receiver.rx_power[ue_list] = np.array(self.transmitter.tx_power[bs]) \
-                                    - np.array(self.coupling_loss[bs,ue_list])
             # create a list with base stations that generate interference in ue_list
-            bs_interf = [b for b in bs_all if b not in [bs]]
+            bs_interf = [b for b in bs_active if b not in [bs]]
 
             # calculate intra system interference
-            for ue in ue_list:
-                for bi in bs_interf:
-                    interference = self.transmitter.tx_power[bi] \
-                                    - self.coupling_loss[bi,ue]
-                    self.receiver.rx_interference[ue] = 10*math.log10( \
-                        math.pow(10, 0.1*self.receiver.rx_interference[ue])
-                        + np.sum(np.power(10, 0.1*interference)))
+            for bi in bs_interf:
+                interference = self.bs.tx_power[bi] - self.coupling_loss_imt[bi,ue] \
+                                - self.param_imt.ue_body_loss - self.param_imt.ue_feed_loss - self.param_imt.bs_feed_loss
+                self.ue.rx_interference[ue] = 10*np.log10( \
+                    np.power(10, 0.1*self.ue.rx_interference[ue]) + np.power(10, 0.1*interference))
 
-        self.receiver.thermal_noise = \
-            10*math.log10(self.param.BOLTZMANN_CONSTANT*self.param.noise_temperature) + \
-            10*np.log10(self.receiver.bandwidth * 1e6) + \
-            self.receiver.noise_figure
+        self.ue.thermal_noise = \
+            10*math.log10(self.param_imt.BOLTZMANN_CONSTANT*self.param_imt.noise_temperature) + \
+            10*np.log10(self.num_rb_per_ue*self.param_imt.rb_bandwidth * 1e6) + \
+            self.ue.noise_figure
 
-        self.receiver.total_interference = \
-            10*np.log10(np.power(10, 0.1*self.receiver.rx_interference) + \
-                        np.power(10, 0.1*self.receiver.thermal_noise))
-        self.receiver.sinr = self.receiver.rx_power - self.receiver.total_interference
-        self.receiver.snr = self.receiver.rx_power - self.receiver.thermal_noise
+        self.ue.total_interference = \
+            10*np.log10(np.power(10, 0.1*self.ue.rx_interference) + \
+                        np.power(10, 0.1*self.ue.thermal_noise))
+            
+        self.ue.sinr = self.ue.rx_power - self.ue.total_interference
+        self.ue.snr = self.ue.rx_power - self.ue.thermal_noise
 
-    def collect_results(self):
-        self.results.add_coupling_loss_dl( \
-            np.reshape(self.coupling_loss, self.num_transmitters*self.num_receivers).tolist())
-
-        for bs in range(self.num_transmitters):
-            self.results.add_tx_power_dl(self.transmitter.tx_power[bs])
-
-        # select the active stations
-        ids = np.where(self.receiver.active)
-
-        self.results.add_sinr_dl(self.receiver.sinr[ids].tolist())
-        self.results.add_snr_dl(self.receiver.snr[ids].tolist())
+        
+    def collect_results(self, write_to_file: bool, snapshot_number: int):
+        #self.results.system_inr.extend([self.system.inr])
+        
+        bs_active = np.where(self.bs.active)[0]
+        for bs in bs_active:
+            ue = self.link[bs]
+            self.results.imt_coupling_loss.extend(self.coupling_loss_imt[bs,ue])
+            self.results.imt_bs_antenna_gain.extend(self.imt_bs_antenna_gain[bs,ue])
+            #tput = self.calculate_imt_ul_tput(self.bs.sinr[bs])
+            #self.results.imt_ul_tput.extend(tput.tolist())
+            self.results.imt_dl_tx_power.extend(self.bs.tx_power[bs].tolist())
+            #imt_dl_tx_power_density = 10*np.log10(np.power(10, 0.1*self.bs.tx_power[bs])/(self.num_rb_per_ue*self.param_imt.rb_bandwidth*1e6))
+            #self.results.imt_dl_tx_power_density.extend(imt_dl_tx_power_density.tolist())
+            self.results.imt_dl_sinr.extend(self.ue.sinr[ue].tolist())
+            self.results.imt_dl_snr.extend(self.ue.snr[ue].tolist())
+            
+        if write_to_file:
+            self.results.write_files(snapshot_number)
+            self.notify_observers(source=__name__, results=self.results)
 
---
+++ tests/test_simulation_downlink.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 16:22:42 2017
+
+@author: edgar
+"""
+
+import unittest
+import numpy as np
+import numpy.testing as npt
+import math
+
+from sharc.simulation_downlink import SimulationDownlink
+from sharc.parameters.parameters_imt import ParametersImt
+from sharc.parameters.parameters_fss import ParametersFss
+from sharc.parameters.parameters_antenna_imt import ParametersAntennaImt
+from sharc.antenna.antenna_omni import AntennaOmni
+from sharc.station_factory import StationFactory
+
+class SimulationDownlinkTest(unittest.TestCase):
+
+    def setUp(self):
+        self.param = ParametersImt()
+        self.param.topology = "SINGLE_BS"
+        self.param.num_macrocell_sites = 19
+        self.param.num_clusters = 2
+        self.param.intersite_distance = 200
+        self.param.minimum_separation_distance_bs_ue = 10
+        self.param.interfered_with = False
+        self.param.frequency = 10000
+        self.param.bandwidth = 100
+        self.param.rb_bandwidth = 0.180
+        self.param.guard_band_ratio = 0.1
+        self.param.ho_margin = 3
+        self.param.bs_load_probability = 1
+        self.param.num_resource_blocks = 10
+        self.param.bs_tx_power = 40
+        self.param.bs_height = 6
+        self.param.bs_aclr = 40
+        self.param.bs_acs = 30
+        self.param.bs_noise_figure = 7
+        self.param.bs_noise_temperature = 290
+        self.param.bs_feed_loss = 3
+        self.param.ul_attenuation_factor = 0.4
+        self.param.ul_sinr_min = -10
+        self.param.ul_sinr_max = 22
+        self.param.ue_k = 2
+        self.param.ue_k_m = 1
+        self.param.ue_indoor_percent = 0
+        self.param.ue_tx_power_control = "OFF"
+        self.param.ue_tx_power_target = -95
+        self.param.ue_tx_power_alfa = 0.8
+        self.param.ue_tx_power = 20
+        self.param.ue_height = 1.5
+        self.param.ue_aclr = 35
+        self.param.ue_acs = 25
+        self.param.ue_noise_figure = 9
+        self.param.ue_feed_loss = 3
+        self.param.ue_body_loss = 4
+        self.param.dl_attenuation_factor = 0.6
+        self.param.dl_sinr_min = -10
+        self.param.dl_sinr_max = 30
+        self.param.channel_model = "FSPL"
+        self.param.line_of_sight_prob = 0.75 # probability of line-of-sight (not for FSPL)
+        self.param.noise_temperature = 290
+        self.param.BOLTZMANN_CONSTANT = 1.38064852e-23
+        
+        self.param_ant = ParametersAntennaImt()
+        self.param_ant.bs_tx_element_max_g = 10
+        self.param_ant.bs_tx_element_phi_3db = 80
+        self.param_ant.bs_tx_element_theta_3db = 80
+        self.param_ant.bs_tx_element_am = 25
+        self.param_ant.bs_tx_element_sla_v = 25
+        self.param_ant.bs_tx_n_rows = 16
+        self.param_ant.bs_tx_n_columns = 16
+        self.param_ant.bs_tx_element_horiz_spacing = 1
+        self.param_ant.bs_tx_element_vert_spacing = 1
+        self.param_ant.bs_rx_element_max_g = 5
+        self.param_ant.bs_rx_element_phi_3db = 65
+        self.param_ant.bs_rx_element_theta_3db = 65
+        self.param_ant.bs_rx_element_am = 30
+        self.param_ant.bs_rx_element_sla_v = 30
+        self.param_ant.bs_rx_n_rows = 2
+        self.param_ant.bs_rx_n_columns = 2
+        self.param_ant.bs_rx_element_horiz_spacing = 0.5
+        self.param_ant.bs_rx_element_vert_spacing = 0.5
+        self.param_ant.ue_tx_element_max_g = 5
+        self.param_ant.ue_tx_element_phi_3db = 65
+        self.param_ant.ue_tx_element_theta_3db = 65
+        self.param_ant.ue_tx_element_am = 30
+        self.param_ant.ue_tx_element_sla_v = 30
+        self.param_ant.ue_tx_n_rows = 2
+        self.param_ant.ue_tx_n_columns = 1
+        self.param_ant.ue_tx_element_horiz_spacing = 0.5
+        self.param_ant.ue_tx_element_vert_spacing = 0.5
+        self.param_ant.ue_rx_element_max_g = 10
+        self.param_ant.ue_rx_element_phi_3db = 90
+        self.param_ant.ue_rx_element_theta_3db = 90
+        self.param_ant.ue_rx_element_am = 25
+        self.param_ant.ue_rx_element_sla_v = 25
+        self.param_ant.ue_rx_n_rows = 16
+        self.param_ant.ue_rx_n_columns = 16
+        self.param_ant.ue_rx_element_horiz_spacing = 1
+        self.param_ant.ue_rx_element_vert_spacing = 1
+        
+        self.param_service = ParametersFss()
+        self.param_service.frequency = 10000
+        self.param_service.bandwidth = 100
+        self.param_service.sat_altitude = 35786000
+        self.param_service.sat_lat_deg = 0
+        self.param_service.sat_noise_temperature = 950
+        self.param_service.sat_interference_noise_ratio = -12.2
+        self.param_service.sat_rx_antenna_gain = 51
+        self.param_service.sat_rx_antenna_pattern = "OMNI"
+        self.param_service.imt_altitude = 1000
+        self.param_service.imt_lat_deg = -23.5629739
+        self.param_service.imt_long_diff_deg = (-46.6555132-75)
+        self.param_service.channel_model = "FSPL"
+        self.param_service.line_of_sight_prob = 0.01
+        self.param_service.surf_water_vapour_density = 7.5
+        self.param_service.specific_gaseous_att = 0.1
+        self.param_service.time_ratio = 0.5
+        self.param_service.sat_rx_antenna_l_s = -20    
+        self.param_service.BOLTZMANN_CONSTANT = 1.38064852e-23
+        self.param_service.EARTH_RADIUS = 6371000        
+
+        self.simulation = SimulationDownlink(self.param, self.param_service, self.param_ant)
+        self.simulation.initialize()
+
+
+    def test_simulation_2bs_4ue(self):
+        self.simulation.bs = StationFactory.generate_imt_base_stations(self.param,
+                                                                       self.param_ant,
+                                                                       self.simulation.topology)
+        self.simulation.bs.antenna = np.array([AntennaOmni(1), AntennaOmni(2)])
+        self.simulation.bs.active = np.ones(2, dtype=bool)
+        
+        self.simulation.ue = StationFactory.generate_imt_ue(self.param,
+                                                            self.param_ant,
+                                                            self.simulation.topology)
+        self.simulation.ue.x = np.array([20, 70, 110, 170])
+        self.simulation.ue.y = np.array([ 0,  0,   0,   0])
+        self.simulation.ue.antenna = np.array([AntennaOmni(10), AntennaOmni(11), AntennaOmni(22), AntennaOmni(23)])
+        self.simulation.ue.active = np.ones(4, dtype=bool)
+        
+        # test connection method
+        self.simulation.connect_ue_to_bs()
+        self.assertEqual(self.simulation.link, {0: [0,1], 1: [2,3]})
+        
+        # We do not test the selection method here because in this specific 
+        # scenario we do not want to change the order of the UE's 
+        #self.simulation.select_ue()
+        
+        # test coupling loss method
+        self.simulation.coupling_loss_imt = self.simulation.calculate_coupling_loss(self.simulation.bs, 
+                                                                                    self.simulation.ue,
+                                                                                    self.simulation.propagation_imt)
+        npt.assert_allclose(self.simulation.coupling_loss_imt, 
+                            np.array([[78.47-1-10,  89.35-1-11,  93.27-1-22,  97.05-1-23], 
+                                      [97.55-2-10,  94.72-2-11,  91.53-2-22,  81.99-2-23]]), 
+                            atol=1e-2)
+        
+        # test scheduler and bandwidth allocation
+        self.simulation.scheduler()
+        bandwidth_per_ue = math.trunc((1 - 0.1)*100/2)       
+        npt.assert_allclose(self.simulation.ue.bandwidth, bandwidth_per_ue*np.ones(4), atol=1e-2)
+        
+        # there is no power control, so BS's will transmit at maximum power
+        self.simulation.power_control()
+        p_tx = 10*math.log10(math.pow(10, 0.1*(40))/2)
+        npt.assert_allclose(self.simulation.bs.tx_power[0], np.array([p_tx, p_tx]), atol=1e-2)
+        npt.assert_allclose(self.simulation.bs.tx_power[1], np.array([p_tx, p_tx]), atol=1e-2)
+        
+        # test method that calculates SINR 
+        self.simulation.calculate_sinr()
+        # check UE received power
+        npt.assert_allclose(self.simulation.ue.rx_power, 
+                            np.array([p_tx-(78.47-1-10)-10, p_tx-(89.35-1-11)-10, p_tx-(91.53-2-22)-10, p_tx-(81.99-2-23)-10]),
+                            atol=1e-2)
+        # check UE received interference
+        npt.assert_allclose(self.simulation.ue.rx_interference, 
+                            np.array([p_tx-(97.55-2-10)-10,  p_tx-(94.72-2-11)-10, p_tx-(93.27-1-22)-10, p_tx-(97.05-1-23)-10]),
+                            atol=1e-2)
+        # check UE thermal noise
+        npt.assert_allclose(self.simulation.ue.thermal_noise, 
+                            10*np.log10(1.38064852e-23*290*bandwidth_per_ue*1e6) + 9,
+                            atol=1e-2)
+        # check BS thermal noise + interference
+        npt.assert_allclose(self.simulation.ue.total_interference, 
+                            10*np.log10(np.power(10, 0.1*np.array([p_tx-(97.55-2-10)-10,  p_tx-(94.72-2-11)-10, p_tx-(93.27-1-22)-10, p_tx-(97.05-1-23)-10])) +
+                                        np.power(10, 0.1*(-120.44))),
+                            atol=1e-2)
+        # check SNR 
+        npt.assert_allclose(self.simulation.ue.snr, 
+                            np.array([p_tx-(78.47-1-10)-10 - (-118.44),  p_tx-(89.35-1-11)-10 - (-118.44), p_tx-(91.53-2-22)-10 - (-118.44),  p_tx-(81.99-2-23)-10 - (-118.44)]),
+                            atol=1e-2)        
+        # check SINR (using only the I term since I >> N in this case)
+        npt.assert_allclose(self.simulation.ue.sinr, 
+                            np.array([-(78.47-1-10) + (97.55-2-10), -(89.35-1-11) + (94.72-2-11), -(91.53-2-22) + (93.27-1-22), -(81.99-2-23) + (97.05-1-23)]),
+                            atol=1e-2)        
+
+#        self.simulation.system = StationFactory.generate_fss_space_stations(self.param_service)
+#        
+#        # test the method that calculates interference from IMT UE to FSS space station
+#        self.simulation.calculate_external_interference()
+#        # check coupling loss
+#        npt.assert_allclose(self.simulation.coupling_loss_imt_system,
+#                            np.array([203.52-51-10, 203.52-51-11, 203.52-51-22, 203.52-51-23]),
+#                            atol=1e-2)
+#        # check interference generated by UE to FSS space station
+#        interference_ue = 20 - np.array([203.52-51-10, 203.52-51-11, 203.52-51-22, 203.52-51-23]) - 7 + 10*math.log10(45/100)
+#        rx_interference = 10*math.log10(np.sum(np.power(10, 0.1*interference_ue)))
+#        self.assertAlmostEqual(self.simulation.system.rx_interference,
+#                               rx_interference,
+#                               delta=.01)
+#        # check FSS space station thermal noise
+#        thermal_noise = 10*np.log10(1.38064852e-23*950*100*1e6)
+#        self.assertAlmostEqual(self.simulation.system.thermal_noise, 
+#                               thermal_noise,
+#                               delta=.01)      
+#        # check INR at FSS space station
+#        self.assertAlmostEqual(self.simulation.system.inr, 
+#                               rx_interference - thermal_noise,
+#                               delta=.01)        
+
+if __name__ == '__main__':
+    unittest.main()
