Updating calculate_external_interference method. bug fix in noise floor calculation for space station.

sharc/simulation_uplink.py
@@ -145,15 +145,14 @@ class SimulationUplink(Simulation):
                                                                      self.ue,
                                                                      self.propagation_system)
 
-        ue_bandwidth = self.num_rb_per_ue * self.param_imt.rb_bandwidth
-
         # applying a bandwidth scaling factor since UE transmits on a portion
         # of the satellite's bandwidth
         # calculate interference only from active UE's
         ue_active = np.where(self.ue.active)[0]
-        interference_ue = self.ue.tx_power[ue_active] - self.coupling_loss_imt_system[ue_active] \
-                            + 10*math.log10(ue_bandwidth/self.param_system.bandwidth) \
-                            - self.param_imt.ue_body_loss - self.param_imt.ue_feed_loss
+        interference_ue = self.ue.tx_power[ue_active] \
+                            - self.param_imt.ue_feed_loss - self.param_imt.ue_body_loss \
+                            - self.coupling_loss_imt_system[ue_active] \
+                            + 10*np.log10(self.ue.bandwidth[ue_active]/self.param_system.bandwidth) \
 
         # calculate the aggregate interference on system
         self.system.rx_interference = 10*math.log10(np.sum(np.power(10, 0.1*interference_ue)))
@@ -161,7 +160,7 @@ class SimulationUplink(Simulation):
         # calculate N
         self.system.thermal_noise = \
             10*math.log10(self.param_system.BOLTZMANN_CONSTANT* \
-                          self.param_system.sat_noise_temperature) + \
+                          self.param_system.sat_noise_temperature*1e3) + \
                           10*math.log10(self.param_system.bandwidth * 1e6)
 
         # calculate INR at the system

tests/test_simulation_uplink.py
@@ -235,13 +235,13 @@ class SimulationUplinkTest(unittest.TestCase):
                                rx_interference,
                                delta=.01)
         # check FSS space station thermal noise
-        thermal_noise = 10*np.log10(1.38064852e-23*950*100*1e6)
+        thermal_noise = 10*np.log10(1.38064852e-23*950*100*1e3*1e6)
         self.assertAlmostEqual(self.simulation.system.thermal_noise, 
                                thermal_noise,
                                delta=.01)      
         # check INR at FSS space station
         self.assertAlmostEqual(self.simulation.system.inr, 
-                               rx_interference - thermal_noise,
+                               -117.18 - (-88.82),
                                delta=.01)        
         
         