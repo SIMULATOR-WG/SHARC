764562459c50d391378d09b1f960dda8048f3dbf
edgar <edgar@anatel.gov.br>
2017-07-27 15:33:46 -0300

    Refactoring class to use numpy arrays as input parameters

--- tests/test_propagation_free_space.py
+++ tests/test_propagation_free_space.py
@@ -7,36 +7,40 @@ Created on Mon Mar  13 15:14:34 2017
 
 import unittest
 import numpy as np
+import numpy.testing as npt
 
 from sharc.propagation.propagation_free_space import PropagationFreeSpace
 
 class PropagationFreeSpaceTest(unittest.TestCase):
     
     def setUp(self):
-        self.__freeSpace = PropagationFreeSpace()
+        self.freeSpace = PropagationFreeSpace()
         
     def test_loss(self):
-        d = 10
-        f = 10
-        self.assertEqual(12.45, 
-                         self.__freeSpace.get_loss(distance_2D=d, frequency=f))
+        d = np.array([10])
+        f = np.array([10])
+        loss = self.freeSpace.get_loss(distance_2D=d, frequency=f)
+        ref_loss = np.array([12.45])
+        npt.assert_allclose(ref_loss, loss, atol=1e-2)
 
-        d = [ 10, 100 ]
-        f = [ 10, 100 ]
-        self.assertTrue(np.all(np.equal([12.45, 52.45], 
-                         self.__freeSpace.get_loss(distance_2D=d, frequency=f))))
+        d = np.array([ 10, 100 ])
+        f = np.array([ 10, 100 ])
+        loss = self.freeSpace.get_loss(distance_2D=d, frequency=f)
+        ref_loss = np.array([12.45, 52.45])
+        npt.assert_allclose(ref_loss, loss, atol=1e-2)
 
-        d = [ 10, 100, 1000 ]
-        f = [ 10, 100, 1000 ]
-        self.assertTrue(np.all(np.equal([12.45, 52.45, 92.45], 
-                         self.__freeSpace.get_loss(distance_2D=d, frequency=f))))
+        d = np.array([ 10, 100, 1000 ])
+        f = np.array([ 10, 100, 1000 ])
+        loss = self.freeSpace.get_loss(distance_2D=d, frequency=f)
+        ref_loss = np.array([12.45, 52.45, 92.45])
+        npt.assert_allclose(ref_loss, loss, atol=1e-2)
 
-        d = [[10, 20, 30],[40, 50, 60]]
-        f = [ 100 ]
-        ref_loss = [[ 32.45,  38.47,  41.99],
-                    [ 44.49,  46.42,  48.01]]
-        loss = self.__freeSpace.get_loss(distance_2D=d, frequency=f)
-        self.assertTrue(np.all(np.isclose(ref_loss, loss, atol=1e-2)))         
+        d = np.array([[10, 20, 30], [40, 50, 60]])
+        f = np.array([ 100 ])
+        loss = self.freeSpace.get_loss(distance_2D=d, frequency=f)
+        ref_loss = np.array([[ 32.45,  38.47,  41.99],
+                             [ 44.49,  46.42,  48.01]])
+        npt.assert_allclose(ref_loss, loss, atol=1e-2)
         
         
 if __name__ == '__main__':
