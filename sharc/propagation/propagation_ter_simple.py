de8accd06772e806fa438d5c40e960ef4921e3ea
edgar <edgar@anatel.gov.br>
2017-08-24 19:48:15 -0300

    Refactoring clutter loss implementation

--- sharc/propagation/propagation_ter_simple.py
+++ sharc/propagation/propagation_ter_simple.py
@@ -37,72 +37,55 @@ class PropagationTerSimple(Propagation):
         
         free_space_loss = self.free_space.get_loss(distance_2D = d,
                                                    frequency = f)
-        clutter_loss_1 = np.zeros(d.shape)
-        clutter_loss_2 = np.zeros(d.shape)
         
-        # TODO: this implementation assumes that there is only one station of 
-        # the other system and n IMT stations, so dimensions of the matrices
-        # will always be 1 x n. This has to be generalized
-        
-        # when 250 < d < 1000, correction is applied at only one end of the path
-        id_1 = np.where(d > 250)[1]
-        if len(id_1):
-            clutter_loss_1[0,id_1] = self.clutter.get_loss(frequency = f[0,id_1],
-                                                         distance_2D = d[0,id_1],
-                                                         loc_percentage = p,
-                                                         station_type = StationType.FSS_ES)
-            
-        # when d > 1000, correction is applied at both ends of the path
-        id_2 = np.where(d > 1000)[1]
-        if len(id_2):
-            clutter_loss_2[0,id_2] = self.clutter.get_loss(frequency = f[0,id_2],
-                                                         distance_2D = d[0,id_2],
-                                                         loc_percentage = p,
-                                                         station_type = StationType.FSS_ES)
+        clutter_loss = self.clutter.get_loss(frequency = f,
+                                             distance_2D = d,
+                                             loc_percentage = p,
+                                             station_type = StationType.FSS_ES)
         
         building_loss = self.building_loss*indoor_stations
         
-        loss = free_space_loss + building_loss + clutter_loss_1 + clutter_loss_2
+        loss = free_space_loss + building_loss + clutter_loss
         
         return loss
         
 
-#if __name__ == '__main__':
-#
-#    import matplotlib.pyplot as plt
-#        
-#    ###########################################################################
-#    # Print path loss for TerrestrialSimple and Free Space
-#    
-#    shadowing_std = 0
-#    d = np.transpose(np.linspace(10, 10000, num=10000)[:,np.newaxis])
-#    freq = 27000*np.ones(d.shape)
-#    indoor_stations = np.zeros(d.shape, dtype = bool)
-#    
-#    free_space = PropagationFreeSpace()
-#    ter_simple = PropagationTerSimple()
-#    
-#    loss_ter = ter_simple.get_loss(distance_2D = d, 
-#                                  frequency = freq,
-#                                  loc_percentage = 0.5,
-#                                  indoor_stations = indoor_stations)
-#
-#    loss_fs = free_space.get_loss(distance_2D = d, 
-#                                  frequency = freq)
-#    
-#    fig = plt.figure(figsize=(8,6), facecolor='w', edgecolor='k')
-#
-#    plt.semilogx(np.squeeze(d), np.squeeze(loss_fs), label = "free space")
-#    plt.semilogx(np.squeeze(d), np.squeeze(loss_ter), label = "free space + clutter loss")
-#    
-#    plt.title("Free space with additional median clutter loss ($f=27GHz$)")
-#    plt.xlabel("distance [m]")
-#    plt.ylabel("path loss [dB]")
-#    plt.xlim((0, d[-1,0]))
-#    plt.ylim((80, 240))                
-#    plt.legend(loc="upper left")
-#    plt.tight_layout()    
-#    plt.grid()
-#
-#    plt.show()
+if __name__ == '__main__':
+
+    import matplotlib.pyplot as plt
+        
+    ###########################################################################
+    # Print path loss for TerrestrialSimple and Free Space
+    
+    d = np.linspace(10, 10000, num=10000)
+    freq = 27000*np.ones(d.shape)
+    indoor_stations = np.zeros(d.shape, dtype = bool)
+    loc_percentage = 0.5
+    
+    free_space = PropagationFreeSpace()
+    ter_simple = PropagationTerSimple()
+    
+    loss_ter = ter_simple.get_loss(distance_2D = d, 
+                                  frequency = freq,
+                                  loc_percentage = loc_percentage,
+                                  indoor_stations = indoor_stations)
+
+    loss_fs = free_space.get_loss(distance_2D = d, 
+                                  frequency = freq)
+    
+    fig = plt.figure(figsize=(8,6), facecolor='w', edgecolor='k')
+
+    plt.semilogx(np.squeeze(d), np.squeeze(loss_fs), label = "free space")
+    plt.semilogx(np.squeeze(d), np.squeeze(loss_ter), label = "free space + clutter loss")
+    
+    plt.title("Free space with additional median clutter loss ($f=27GHz$)")
+    plt.xlabel("distance [m]")
+    plt.ylabel("path loss [dB]")
+    plt.xlim((0, d[-1]))
+    plt.ylim((80, 240))                
+    plt.legend(loc="upper left")
+    plt.tight_layout()    
+    plt.grid()
+
+    plt.show()
             
