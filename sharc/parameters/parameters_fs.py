fdb5f3092078a929437d4165b33727bc1729169c
edgar <edgar@anatel.gov.br>
2017-09-11 13:08:56 -0300

    New parameters for fixed services

---
+++ sharc/parameters/parameters_fs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 17:52:16 2017
+
+@author: edgar
+"""
+
+class ParametersFs(object):
+    """
+    Simulation parameters for Fixed Wireless Systems.
+    """
+    
+    def __init__(self):
+        pass
