9f83aad66becbff6d513d2fec8061996f072fb75
CalilQ <calil_queiroz@hotmail.com>
2017-11-16 19:19:55 -0200

    Implement calculate_gain method

--- sharc/antenna/antenna_sa509.py
+++ sharc/antenna/antenna_sa509.py
@@ -32,5 +32,31 @@ class AntenaSA509(Antenna):
         self.phi_1 = self.phi_0*np.sqrt(20/3)
         self.phi_2 = 10**((49-self.g_0)/25)
         
-    def calculate_gain(self):
-        pass
+    def calculate_gain(self, *args, **kwargs) -> np.array:
+        phi = np.absolute(kwargs["phi_vec"])
+        
+        gain = np.zeros_like(phi)
+        
+        # First part
+        interval_idx = np.where(np.logical_and(phi >= 0, phi < self.phi_1))
+        gain[interval_idx] = self.g_0 - 3*(phi[interval_idx]/self.phi_0)**2
+        # Second part
+        interval_idx = np.where(np.logical_and(phi >= self.phi_1, phi < self.phi_2))
+        gain[interval_idx] = self.g_0 - 20
+        # Third part
+        interval_idx = np.where(np.logical_and(phi >= self.phi_2, phi < 48))
+        gain[interval_idx] = 29 - 25*np.log10(phi(interval_idx))
+        # Fourth part
+        interval_idx = np.where(np.logical_and(phi >= 48, phi < 80))
+        gain[interval_idx] = -13
+        # Fifth part
+        interval_idx = np.where(np.logical_and(phi >= 80, phi < 120))
+        gain[interval_idx] = -8
+        # Sixth part
+        interval_idx = np.where(np.logical_and(phi >= 120, phi < 180))
+        gain[interval_idx] = -13
+        
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+    phi = np.linspace(0.1, 180, num = 100000)
