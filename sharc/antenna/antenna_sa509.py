95b5ce96d2110988993e154b0720122318e20cad
CalilQ <calil_queiroz@hotmail.com>
2017-11-17 15:05:35 -0200

    Implement radiation pattern plot

--- sharc/antenna/antenna_sa509.py
+++ sharc/antenna/antenna_sa509.py
@@ -53,12 +53,44 @@ class AntenaSA509(Antenna):
         interval_idx = np.where(np.logical_and(phi >= 80, phi < 120))
         gain[interval_idx] = -8
         # Sixth part
-        interval_idx = np.where(np.logical_and(phi >= 120, phi < 180))
+        interval_idx = np.where(np.logical_and(phi >= 120, phi <= 180))
         gain[interval_idx] = -13
         
         return gain
         
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    
+    par = ParametersRas();
+    par.diameter = 1
+    par.antenna_efficiency = 1
+    par.frequency = 43000
+    par.SPEED_OF_LIGHT = 3e8
+        
+    antenna1 = AntenaSA509(par)
+    par.diameter = 7
+    antenna7 = AntenaSA509(par)
+    par.diameter = 10
+    antenna10 = AntenaSA509(par)
 
     phi = np.linspace(0.1, 180, num = 100000)
+    gain1  = antenna1.calculate_gain(phi_vec = phi)
+    gain7  = antenna7.calculate_gain(phi_vec = phi)
+    gain10  = antenna10.calculate_gain(phi_vec = phi)
+    
+    fig = plt.figure(figsize=(8,7), facecolor='w', edgecolor='k')  # create a figure object
+    
+    plt.semilogx(phi, gain1, "-b", label = "$f = 43$ $GHz,$ $D = 1$ $m$")
+    plt.semilogx(phi, gain7, "-r", label = "$f = 43$ $GHz,$ $D = 7$ $m$")
+    plt.semilogx(phi, gain10, "-k", label = "$f = 43$ $GHz,$ $D = 10$ $m$")
+    
+    plt.title("ITU-R SA.509-3 antenna radiation pattern")
+    plt.xlabel("Off-axis angle $\phi$ [deg]")
+    plt.ylabel("Gain [dBi]")
+    plt.legend(loc="lower left")
+    plt.xlim((phi[0], phi[-1]))
+
+    plt.grid()
+    plt.show()  
+    
+    
