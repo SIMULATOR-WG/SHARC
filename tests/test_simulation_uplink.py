d8626bef90618788dc2da403ade47a77814e8472
CalilQ <calil_queiroz@hotmail.com>
2017-11-22 15:42:03 -0200

    Add RAS parameters to test_simulation_uplink.py

--- tests/test_simulation_uplink.py
+++ tests/test_simulation_uplink.py
@@ -144,7 +144,26 @@ class SimulationUplinkTest(unittest.TestCase):
         self.param.fss_es.channel_model = "FSPL"
         self.param.fss_es.line_of_sight_prob = 1 
         self.param.fss_es.BOLTZMANN_CONSTANT = 1.38064852e-23
-        self.param.fss_es.EARTH_RADIUS = 6371000  
+        self.param.fss_es.EARTH_RADIUS = 6371000
+        
+        self.param.ras.x = -5000
+        self.param.ras.y = 0
+        self.param.ras.height = 10
+        self.param.ras.elevation = 20
+        self.param.ras.azimuth = 0
+        self.param.ras.frequency = 10000
+        self.param.ras.bandwidth = 100
+        self.param.ras.antenna_noise_temperature = 50
+        self.param.ras.receiver_noise_temperature = 50
+        self.param.ras.antenna_gain = 50
+        self.param.ras.antenna_efficiency = 0.7
+        self.param.ras.diameter = 10
+        self.param.ras.antenna_pattern = "OMNI"
+        self.param.ras.channel_model = "FSPL"
+        self.param.ras.line_of_sight_prob = 1 
+        self.param.ras.BOLTZMANN_CONSTANT = 1.38064852e-23
+        self.param.ras.EARTH_RADIUS = 6371000
+        self.param.ras.SPEED_OF_LIGHT = 299792458
         
         
     def test_simulation_2bs_4ue_ss(self):
