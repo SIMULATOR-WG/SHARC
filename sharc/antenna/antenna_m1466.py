cb5a50699c5e876ac65a99909df7c0b5d4150c82
edgar <edgar@anatel.gov.br>
2017-12-21 16:49:23 -0200

    New constructor and new method to translate azimuth and elevations angles to local coordinates system.

--- sharc/antenna/antenna_m1466.py
+++ sharc/antenna/antenna_m1466.py
@@ -15,9 +15,22 @@ class AntennaM1466(Antenna):
     Rec. ITU-R M.1466-1
     """
     
-    def __init__(self):
+    def __init__(self, gain, azimuth: float, elevation: float):
+        """
+        Constructs an AntennaM1466 object.
+        Does not receive angles in local coordinate system.
+        Elevation taken wrt x-y plane.
+        
+        Parameters
+        ---------
+            azimuth (float): antenna's physical azimuth inclination
+            elevation (float): antenna's physical elevation inclination
+                wrt x-y plane
+        """
         super().__init__()
-        self.peak_gain = 30
+        self.peak_gain = gain
+        self.azimuth = azimuth
+        self.elevation = elevation
 
         
     def calculate_gain(self, *args, **kwargs) -> np.array:
@@ -34,14 +47,19 @@ class AntennaM1466(Antenna):
             gain (np.array): gain corresponding to each of the given directions
         """
         phi = np.asarray(kwargs["phi_vec"])
-        theta = np.asarray(kwargs["theta_vec"])        
+        theta = np.asarray(kwargs["theta_vec"])
         
-        gain = self.peak_gain + self.get_gain_az(phi) + self.get_gain_elev(theta)
+        phi_l, theta_l = self.to_local_coord(phi, theta)
+        
+        gain = self.peak_gain + self.get_gain_az(phi_l) + self.get_gain_elev(theta_l)
         
         return gain
         
         
     def get_gain_az(self, phi: np.array) -> np.array:
+        """
+        Returns the antenna gain in the azimuth plane for the given direction.
+        """
         pa = np.abs(phi)
         gain = np.empty(pa.shape)
         gain[pa < 0.5] = 0
@@ -53,6 +71,9 @@ class AntennaM1466(Antenna):
     
     
     def get_gain_elev(self, theta: np.array) -> np.array:
+        """
+        Returns the antenna gain in the elevation plane for the given direction.
+        """
         gain = np.empty(theta.shape)
         gain[theta < -60] = -35
         gain[(theta >= -60) & (theta < -30)] = -27.5
@@ -62,6 +83,18 @@ class AntennaM1466(Antenna):
         return gain
         
         
+    def to_local_coord(self, phi: np.array, theta: np.array) -> tuple:
+        """
+        Converts the azimuth and elevation angles to the local coordinate system,
+        taking into account the physical orientation of the antenna.
+        """
+        
+        phi_l = phi - self.azimuth
+        theta_l = 90 - theta - self.elevation
+        
+        return phi_l, theta_l
+
+        
         
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -69,10 +102,14 @@ if __name__ == '__main__':
     azimuth = np.linspace(-90, 90, num = 100000)
     elevation = np.linspace(-80, 20, num = 100000)
     
-    antenna = AntennaM1466()
+    gain = 30
+    azimuth_l = 0
+    elevation_l = 0
     
-    gain_az = antenna.calculate_gain(phi_vec = azimuth, theta_vec = 0)
-    gain_elev = antenna.calculate_gain(phi_vec = 0, theta_vec = elevation)
+    antenna = AntennaM1466(gain, azimuth_l, elevation_l)
+    
+    gain_az = gain + antenna.get_gain_az(azimuth)
+    gain_elev = gain + antenna.get_gain_elev(elevation)
 
     fig = plt.figure(figsize=(15,5), facecolor='w', edgecolor='k')
     #plt.title("Rec. ITU-R M.1466 antenna pattern")
