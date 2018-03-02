5fb1f1c0d80aad8341a26f8f4cdb5056071e3a93
edgar <edgar@anatel.gov.br>
2018-03-02 15:03:28 -0300

    Commenting code that creates figure

--- tests/test_propagation_ducting_ref.py
+++ tests/test_propagation_ducting_ref.py
@@ -57,70 +57,70 @@ class PropagationDuctingReflectionTest(unittest.TestCase):
 #                         self.__dutingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm))))
 
    #Grafico da perda de difraçao em funçao da distancia e da frequencia
-        data1 = []
-        data2 = []
-        data3 = []
-        data4 = []
-        data5 = []
-        eixo_x = []
+#        data1 = []
+#        data2 = []
+#        data3 = []
+#        data4 = []
+#        data5 = []
+#        eixo_x = []
+#
+#
+#
+#        for n in range (1,6,1):
+#
+#            if (n==1):
+#                f = 5000
+#
+#                for d in range(1000, 100100,1000):
+#                    loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
+#
+#                    data1.append(loss)
+#                    eixo_x.append(d/1000)
+#
+#            if (n==2):
+#                f = 10000
+#
+#                for d in range(1000, 100100,1000):
+#                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
+#                     data2.append(loss)
+#
+#            if (n==3):
+#                f = 20000
+#
+#                for d in range(1000, 100100,1000):
+#                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
+#                     data3.append(loss)
+#
+#            if (n==4):
+#                f = 30000
+#
+#                for d in range(1000, 100100,1000):
+#                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
+#                     data4.append(loss)
+#
+#
+#            if (n==5):
+#                f = 40000
+#
+#                for d in range(1000, 100100,1000):
+#                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
+#                     data5.append(loss)
 
-
-
-        for n in range (1,6,1):
-
-            if (n==1):
-                f = 5000
-
-                for d in range(1000, 100100,1000):
-                    loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
-
-                    data1.append(loss)
-                    eixo_x.append(d/1000)
-
-            if (n==2):
-                f = 10000
-
-                for d in range(1000, 100100,1000):
-                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
-                     data2.append(loss)
-
-            if (n==3):
-                f = 20000
-
-                for d in range(1000, 100100,1000):
-                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
-                     data3.append(loss)
-
-            if (n==4):
-                f = 30000
-
-                for d in range(1000, 100100,1000):
-                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
-                     data4.append(loss)
-
-
-            if (n==5):
-                f = 40000
-
-                for d in range(1000, 100100,1000):
-                     loss = self.__ductingRef.get_loss(distance=d, frequency=f,atmospheric_pressure=Ph, air_temperature= T, water_vapour=ro, Dlt = Dlt, Dlr=Dlr, Dct=Dct, Dcr=Dcr, Hts=Hts, Hrs=Hrs, Hte=Hte, Hre=Hre, theta_tx = thetaT, theta_rx = thetaR, N0 = N0, delta_N = deltaN, percentage_p = p, omega=omega, phi=phi, dtm=dtm, dlm=dlm, epsilon=epsilon, hm=hm)
-                     data5.append(loss)
-
-        fig = plt.figure(3)
-        f  = ['5 GHz','10 GHz','20 GHz','30 GHz','40 GHz']
-        ax = fig.add_subplot(111)
-        ax.plot(eixo_x, data1)
-        ax.plot(eixo_x, data2)
-        ax.plot(eixo_x, data3)
-        ax.plot(eixo_x, data4)
-        ax.plot(eixo_x, data5)
-
-        # Add legend, title and axis labels
-        lgd = ax.legend( [ 'f = ' + str(lag) for lag in f], loc='upper center', bbox_to_anchor=(0.8, 0.45))
-        ax.set_title('Ducting/reflection attenuation')
-        ax.set_xlabel('Distance (Km)')
-        ax.set_ylabel('Attenuation (dB)')
-        ax.set_xlim([0,100])
-        ax.set_ylim([200,380])
-        ax.grid(True)
-        fig.savefig('ducting_ref_att.png', dpi=350, format='png')
+#        fig = plt.figure(3)
+#        f  = ['5 GHz','10 GHz','20 GHz','30 GHz','40 GHz']
+#        ax = fig.add_subplot(111)
+#        ax.plot(eixo_x, data1)
+#        ax.plot(eixo_x, data2)
+#        ax.plot(eixo_x, data3)
+#        ax.plot(eixo_x, data4)
+#        ax.plot(eixo_x, data5)
+#
+#        # Add legend, title and axis labels
+#        lgd = ax.legend( [ 'f = ' + str(lag) for lag in f], loc='upper center', bbox_to_anchor=(0.8, 0.45))
+#        ax.set_title('Ducting/reflection attenuation')
+#        ax.set_xlabel('Distance (Km)')
+#        ax.set_ylabel('Attenuation (dB)')
+#        ax.set_xlim([0,100])
+#        ax.set_ylim([200,380])
+#        ax.grid(True)
+#        fig.savefig('ducting_ref_att.png', dpi=350, format='png')
