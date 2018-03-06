7918f87a664ab40ea06ba839134d58190dfb84dc
CalilQ <calil_queiroz@hotmail.com>
2018-03-06 15:34:51 -0300

    Write calculate_correction_factor initial tests
    Result still different from expected value though

--- sharc/antenna/beamforming_normalization/beamforming_normalization.py
+++ sharc/antenna/beamforming_normalization/beamforming_normalization.py
@@ -26,8 +26,8 @@ class BeamformingNormalization(object):
         self.phi_max = 180
         self.theta_min = 0
         self.theta_max = 180
-        self.phi_vals = np.arange(-180,180,res)
-        self.theta_vals = np.arange(0,180,res)
+        self.phi_vals = np.arange(self.phi_min,self.phi_max,res)
+        self.theta_vals = np.arange(self.theta_min,self.theta_max,res)
         self.antenna = None  
                
     def generate_correction_matrix(self, par: AntennaPar, c_chan: bool, file_name: str):
@@ -53,7 +53,6 @@ class BeamformingNormalization(object):
             cf = self.calculate_correction_factor(0,0,c_chan)
           
         # Convert to dB and save
-        cf = 10*np.log10(cf)
         self._save_files(cf,par,file_name)
             
     def calculate_correction_factor(self, phi_e: float, theta_t: float, c_chan: bool):
@@ -63,14 +62,15 @@ class BeamformingNormalization(object):
         if c_chan:
             self.antenna.add_beam(phi_e,theta_t)
             beam = np.array([len(self.antenna.beams_list) - 1])
-            int_f = lambda t,p: np.power(10,self.antenna._beam_gain(p,t,beam)/10)*np.sin(t)
+            int_f = lambda t,p: np.power(10,self.antenna._beam_gain(p,t,beam)/10)*np.sin(np.deg2rad(t))
         else:
-            int_f = lambda t,p: np.power(10,self.element.element_pattern(p,t)/10)*np.sin(t)
+            int_f = lambda t,p: np.power(10,self.antenna.element.element_pattern(p,t)/10)*np.sin(np.deg2rad(t))
         
-        int_val = dblquad(int_f,self.phi_min,self.phi_max,
-                          lambda t: self.theta_min, lambda t: self.theta_max)
+        int_val, err = dblquad(int_f,self.phi_min,self.phi_max,
+                          lambda p: self.theta_min, lambda p: self.theta_max)
         
-        return int_val/(4*np.pi)
+        cf = 10*np.log10(int_val/(4*np.pi))
+        return cf
     
     def _save_files(self, correction, par:AntennaPar, file_name: str):
         """
--- tests/test_beamforming_normalization.py
+++ tests/test_beamforming_normalization.py
@@ -10,6 +10,8 @@ import numpy as np
 import numpy.testing as npt
 
 from sharc.antenna.beamforming_normalization.beamforming_normalization import BeamformingNormalization
+from sharc.support.named_tuples import AntennaPar
+from sharc.antenna.antenna_beamforming_imt import AntennaBeamformingImt
 
 class BeamformingNormalizationTest(unittest.TestCase):
     
@@ -17,6 +19,28 @@ class BeamformingNormalizationTest(unittest.TestCase):
         # Test 1
         resolution = 30
         self.norm_1 = BeamformingNormalization(resolution)
+        element_pattern = "M2101"
+        element_max_g = 5
+        element_phi_deg_3db = 65
+        element_theta_deg_3db = 65
+        element_am = 30
+        element_sla_v = 30
+        n_rows = 8
+        n_columns = 8
+        horiz_spacing = 0.5
+        vert_spacing = 0.5
+        down_tilt = 0
+        self.par_1 = AntennaPar(element_pattern,
+                                element_max_g,
+                                element_phi_deg_3db,
+                                element_theta_deg_3db,
+                                element_am,
+                                element_sla_v,
+                                n_rows,
+                                n_columns,
+                                horiz_spacing,
+                                vert_spacing,
+                                down_tilt)
     
     def test_construction(self):
         # Test 1
@@ -27,11 +51,19 @@ class BeamformingNormalizationTest(unittest.TestCase):
         self.assertEqual(self.norm_1.theta_max,180)
         npt.assert_equal(self.norm_1.phi_vals,np.arange(-180,180,30))
         npt.assert_equal(self.norm_1.theta_vals,np.arange(0,180,30))
-    
-    def test_generate_correction_matrix(self):
-        pass
-    
+        
     def test_calculate_correction_factor(self):
+        # Test 1
+        azi = 0
+        ele = 0
+        self.norm_1.antenna = AntennaBeamformingImt(self.par_1,azi,ele)
+        # Test adjacent channel case: single antenna element
+        c_chan = False
+        c_fac = self.norm_1.calculate_correction_factor(0,0,c_chan)
+        print(c_fac)
+#        self.assertAlmostEqual(c_fac,4.8,delta=1e-2)
+        
+    def test_generate_correction_matrix(self):
         pass
     
     def test_save_files(self):
