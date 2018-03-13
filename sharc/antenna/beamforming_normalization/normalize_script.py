fb2de97e599aab0082e90a75555aa6779b7bb244
CalilQ <calil_queiroz@hotmail.com>
2018-03-13 17:59:03 -0300

    Adapt normalize_script.py to new file save structure

--- sharc/antenna/beamforming_normalization/normalize_script.py
+++ sharc/antenna/beamforming_normalization/normalize_script.py
@@ -18,14 +18,16 @@ tolerance = 1e-2
 norm = BeamformingNormalizer(resolution,tolerance)
 
 # List of antenna parameters to which calculate the normalization factors.
-# Order of parameters is: normalization element_pattern, element_max_g, 
-# element_phi_deg_3db, element_theta_deg_3db, element_am, element_sla_v, 
-# n_rows, n_columns, element_horiz_spacing, element_vert_spacing, downtilt_deg
-param_list = [AntennaPar(False,"M2101",5,65,65,30,30,8,8,0.5,0.5,0),
-              AntennaPar(False,"M2101",5,90,90,25,25,4,4,0.5,0.5,0)]
+# Order of parameters is: normalization, norm_file, element_pattern, 
+# element_max_g, element_phi_deg_3db, element_theta_deg_3db, element_am, 
+# element_sla_v, n_rows, n_columns, element_horiz_spacing, 
+# element_vert_spacing, downtilt_deg
+param_list = [AntennaPar(False,None,"M2101",5,65,65,30,30,8,8,0.5,0.5,0),
+              AntennaPar(False,None,"M2101",5,90,90,25,25,4,4,0.5,0.5,0)]
 co_channel_list = [False, False]
+file_names = ['bs_norm.npz',
+              'ue_norm.npz']
 
 #%% Normalize and save using parameters hash
-for par, c_chan in zip(param_list,co_channel_list):
-    file_name = str(hash((c_chan,par)))
-    cf, err = norm.generate_correction_matrix(par,c_chan,file_name)
+for par, c_chan, file in zip(param_list,co_channel_list,file_names):
+    norm.generate_correction_matrix(par,c_chan,file)
