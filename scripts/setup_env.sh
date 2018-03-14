93ad7a7d7dff21334851eca96e4fc0429eadd43e
CalilQ <calil_queiroz@hotmail.com>
2018-03-14 18:42:45 -0300

    Edit default parameters to use beamforming
    Also improve setup_env.sh script

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -4,9 +4,17 @@ echo "INSTALLING PYTHON, PIP AND GIT"
 sudo apt-get update
 sudo apt-get install python3=3.5.3*
 sudo apt-get install python3-pip
-python3 -m pip install --upgrade pip
+sudo python3 -m pip install --upgrade pip
+sudo apt-get install python-support
+sudo apt-get install python3-tk
 sudo apt-get install git-core
 
 echo "INSTALLING PYTHON PACKAGES"
-python3 -m pip install numpy
-python3 -m pip install shapely
+sudo python3 -m pip install numpy
+sudo python3 -m pip install shapely
+sudo python3 -m pip install scipy
+sudo python3 -m pip install matplotlib
+
+echo "CLONING REPOSITORY"
+git clone https://github.com/MWSL-UnB/FD-SHARC.git
+git pull origin dev_simulation_scripts
