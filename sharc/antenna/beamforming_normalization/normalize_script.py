95ba9ebba5569e89b45ddca36f9b065049cefa92
CalilQ <calil_queiroz@hotmail.com>
2018-03-14 14:01:32 -0300

    Adapt normalize_script.py to dict normalization data

--- sharc/antenna/beamforming_normalization/normalize_script.py
+++ sharc/antenna/beamforming_normalization/normalize_script.py
@@ -18,16 +18,15 @@ tolerance = 1e-2
 norm = BeamformingNormalizer(resolution,tolerance)
 
 # List of antenna parameters to which calculate the normalization factors.
-# Order of parameters is: normalization, norm_file, element_pattern, 
+# Order of parameters is: normalization, norm_data, element_pattern, 
 # element_max_g, element_phi_deg_3db, element_theta_deg_3db, element_am, 
 # element_sla_v, n_rows, n_columns, element_horiz_spacing, 
 # element_vert_spacing, downtilt_deg
 param_list = [AntennaPar(False,None,"M2101",5,65,65,30,30,8,8,0.5,0.5,0),
               AntennaPar(False,None,"M2101",5,90,90,25,25,4,4,0.5,0.5,0)]
-co_channel_list = [False, False]
 file_names = ['bs_norm.npz',
               'ue_norm.npz']
 
-#%% Normalize and save using parameters hash
-for par, c_chan, file in zip(param_list,co_channel_list,file_names):
-    norm.generate_correction_matrix(par,c_chan,file)
+#%% Normalize and save
+for par, file in zip(param_list,file_names):
+    norm.generate_correction_matrix(par,file)
