657f920db914a1139c44396e21f9c2b35ef81a4b
edgar <edgar@anatel.gov.br>
2019-02-27 16:23:18 +0100

    New station type to support EESS passive

--- sharc/support/enumerations.py
+++ sharc/support/enumerations.py
@@ -38,3 +38,4 @@ class StationType(Enum):
     HAPS   = 6  # HAPS (airbone) station
     RNS    = 7  # Radionavigation service
     RAS    = 8  # Radio Astronomy Service
+    EESS_PASSIVE = 9 # EESS passive sensor
